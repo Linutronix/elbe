b6c018aa11100cba8de7bb70d523dae312118e07
Torben Hohn <torbenh@linutronix.de>
2014-02-27 13:33:32 +0100

    dump: Converted elbepack.commands.dump to the new style

---
+++ elbepack/dump.py
@@ -0,0 +1,166 @@
+# ELBE - Debian Based Embedded Rootfilesystem Builder
+# Copyright (C) 2013  Linutronix GmbH
+#
+# This file is part of ELBE.
+#
+# ELBE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ELBE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ELBE.  If not, see <http://www.gnu.org/licenses/>.
+
+from elbepack.asciidoclog import ASCIIDocLog
+from datetime import datetime
+
+def dump_fullpkgs( xml, rfs, cache ):
+    xml.clear_full_pkglist()
+
+    instpkgs = cache.get_installed_pkgs()
+    for p in instpkgs:
+        xml.append_full_pkg( p )
+
+    sources_list = xml.xml.ensure_child( 'sources_list' )
+    slist = rfs.read_file("etc/apt/sources.list")
+    sources_list.set_text( slist )
+
+    try:
+        preferences = xml.xml.ensure_child( 'apt_prefs' )
+        prefs = rfs.read_file("etc/apt/preferences")
+        preferences.set_text(prefs)
+    except:
+        pass
+   
+def do_finetuning(xml, rfs, log):
+    log.printo( "finetuning not implemented" )
+
+def elbe_report( xml, rfs, cache, reportname ):
+    outf = ASCIIDocLog(reportname)
+
+    outf.h1( "ELBE Report for Project " + xml.text("project/name") )
+
+    outf.printo( "report timestamp: "+datetime.now().strftime("%Y%m%d-%H%M%S") )
+
+    slist = rfs.read_file('etc/apt/sources.list')
+    outf.h2( "Apt Sources dump" )
+    outf.verbatim_start()
+    outf.print_raw(slist)
+    outf.verbatim_end()
+
+    try:
+        prefs = rfs.read_file("etc/apt/preferences")
+    except IOError:
+        prefs = ""
+
+    outf.h2( "Apt Preferences dump" )
+    outf.verbatim_start()
+    outf.print_raw(prefs)
+    outf.verbatim_end()
+
+    outf.h2( "Installed Packages List" )
+    outf.table()
+
+    instpkgs = cache.get_installed_pkgs()
+    for p in instpkgs:
+        outf.printo( "|%s|%s|%s" % (p.name, p.installed_version, p.origin) )
+    outf.table()
+
+    # archive extraction is done before and after finetuning the first
+    # extraction is needed that the files can be used (copied/moved to the
+    # buildenv in finetuning
+    # the second extraction is done to ensure that files from the archive
+    # can't be modified/removed in finetuning
+
+    outf.h2( "archive extract before finetuning" )
+
+    if xml.has("archive"):
+        with xml.archive_tmpfile() as fp:
+            outf.do( 'tar xvfj "%s" -C "%s"' % (fp.name, rfs.path) )
+
+    outf.h2( "finetuning log" )
+    outf.verbatim_start()
+
+    index = cache.get_fileindex()
+    mt_index = rfs.mtime_snap()
+    if xml.has("finetuning"):
+        do_finetuning(xml, rfs)
+        #outf.print_raw( do_command( opt.finetuning ) )
+        mt_index_post_fine = rfs.mtime_snap()
+    else:
+        mt_index_post_fine = mt_index
+
+    outf.verbatim_end()
+
+    outf.h2( "archive extract after finetuning" )
+
+    if xml.has("archive"):
+        with xml.archive_tmpfile() as fp:
+            outf.do( 'tar xvfj "%s" -C "%s"' % (fp.name, rfs.path) )
+        mt_index_post_arch = rfs.mtime_snap()
+    else:
+        mt_index_post_arch = mt_index_post_fine
+
+    outf.h2( "fileslist" )
+    outf.table()
+
+    tgt_pkg_list = set()
+
+    for fpath, realpath in rfs.walk_files():
+        if index.has_key(fpath):
+            pkg = index[fpath]
+            tgt_pkg_list.add(pkg)
+        else:
+            pkg = "postinst generated"
+
+        if mt_index_post_fine.has_key(fpath) and mt_index.has_key(fpath):
+            if mt_index_post_fine[fpath] > mt_index[fpath]:
+                pkg = "modified finetuning"
+        if mt_index_post_fine.has_key(fpath):
+            if mt_index_post_arch[fpath] > mt_index_post_fine[fpath]:
+                pkg = "from archive"
+            elif not mt_index.has_key(fpath):
+                pkg = "added in finetuning"
+        else:
+            pkg = "added in archive"
+
+        outf.printo( "|+%s+|%s" % (fpath,pkg) )
+
+    outf.table()
+
+    outf.h2( "Deleted Files" )
+    outf.table()
+    for fpath in mt_index.keys():
+        if not mt_index_post_arch.has_key(fpath):
+            if index.has_key(fpath):
+                pkg = index[fpath]
+            else:
+                pkg = "postinst generated"
+            outf.printo( "|+%s+|%s" % (fpath,pkg) )
+    outf.table()
+
+    outf.h2( "Target Package List" )
+    outf.table()
+    instpkgs = cache.get_installed_pkgs()
+    pkgindex = {}
+    for p in instpkgs:
+        pkgindex[p.name] = p
+
+    if xml.has("target/pkgversionlist"):
+        rfs.remove('etc/elbe_pkglist')
+        f = rfs.open('etc/elbe_pkglist', 'w')
+    for pkg in tgt_pkg_list:
+        p = pkgindex[pkg]
+        outf.printo( "|%s|%s|%s|%s" % (p.name, p.installed_version, p.is_auto_installed, p.installed_md5) )
+        if xml.has("target/pkgversionlist"):
+            f.write ("%s %s %s\n" % (p.name, p.installed_version, p.installed_md5))
+    outf.table()
+
+    if xml.has("target/pkgversionlist"):
+        f.close ()
+
