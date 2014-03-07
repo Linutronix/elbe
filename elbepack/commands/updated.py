b0cb867666c5180ccc041b5761d3d1f6c7cbeb8f
Manuel Traut <manut@linutronix.de>
2014-03-07 16:10:05 +0100

    updated: use pyinotify instead of watchdog and unzip update
     - pyinotify is packaged in debian, watchdog not ..
     - unzip the update.zip and add the repo to the sources list
    
    Signed-off-by: Manuel Traut <manut@linutronix.de>

--- elbepack/commands/updated.py
+++ elbepack/commands/updated.py
@@ -19,45 +19,123 @@
 # along with ELBE.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import pyinotify
 import signal
 import sys
 import time
-
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventFileMonitor
+import threading
 
 from optparse import OptionParser
+from zipfile import (ZipFile, BadZipfile)
 
 from elbepack.treeutils import etree
 from elbepack.xmldefaults import ElbeDefaults
 
-observer = None
-stop = False
-
-class FileMonitor (FileSystemEventFileMonitor):
-    def on_created (self, event):
-        print event
-
-    def on_deleted(self, event):
-        print event
-
-    def on_moved(self, event):
-        print event
-
-def shutdown(signum, fname):
-
-    global observer
-    global stop
-
-    observer.stop ()
-    observer.join ()
+class UpdateStatus:
+    msg = "waiting"
     observer = None
-    stop = True
+    stop = False
+
+status = UpdateStatus ()
+
+def update_sourceslist (xml, update_dir):
+    deb =  "deb file://" + update_dir + " " + xml.text ("/project/suite")
+    deb += " main"
+    fname = "/etc/apt/sources.list.d/"
+    fname += xml.text ("/project/name") + "_" + xml.text ("/project/version")
+
+    with open (fname, 'w') as f:
+        f.write (deb)
+
+def apply_update (xml):
+    # TODO use python-apt (apt.Cache) and fullpkglist to install specified
+    #      packages and remove unmentioned packages that are currently installed
+    pass
+
+def update (upd_file):
+
+    global status
+
+    status.msg = "updating.. %s" % upd_file
+
+    try:
+        upd_file_z = ZipFile (upd_file)
+    except BadZipfile:
+        status.msg = "update aborted (bad zip file: %s)" % upd_file
+        return
+
+    if not "source.xml" in upd_file_z.namelist ():
+        status.msg = "update invalid (source.xml missing)"
+        return
+
+    upd_file_z.extract ("new.xml", "/tmp/")
+
+    xml = etree ("/tmp/new.xml")
+    prefix = "/opt/elbe/" + xml.text ("/project/name")
+    prefix += "_" + xml.text ("/project/version") + "/"
+
+    status.msg = "updating: " + prefix
+
+    for i in upd_file_z.namelist ():
+
+        (dirname, filename) = os.path.split (i)
+        status.msg = "unzip %s: %s" % (prefix+dirname, filename)
+
+        try:
+            if not os.path.exists (prefix+dirname):
+                os.makedirs (prefix+dirname)
+
+            upd_file_z.extract (i, prefix+dirname)
+        except OSError:
+            status.msg = "extraction failed: %s" % sys.exc_info () [1]
+            return
+
+    update_sourceslist (xml, prefix+"/repo")
+    apply_update (xml)
+
+    status.msg = "update done: " + prefix
+
+def action_select (event):
+    action = event.pathname.split ('.') [-1]
+
+    if action == "upd":
+        update (event.pathname)
+    else:
+        print "action_select: unhandled file: %s" % event.pathname
+
+class FileMonitor (pyinotify.ProcessEvent):
+    def process_IN_CLOSE_WRITE (self, event):
+        action_select (event)
+
+def shutdown (signum, fname):
+
+    global status
+
+    status.observer.stop ()
+    status.observer = None
+    status.stop = True
+
+class ObserverThread (threading.Thread):
+
+    def __init__(self):
+                threading.Thread.__init__(self)
+
+    def run (self):
+
+        global status
+
+        while 1:
+            if status.observer.check_events (timeout=1000):
+                status.observer.read_events ()
+                status.observer.process_events ()
+
+            if status.stop:
+                print "terminate thread"
+                return
 
 def run_command (argv):
 
-    global observer
-    global stop
+    global status
 
     oparser = OptionParser (usage="usage: %prog init [options] <filename>")
 
@@ -75,15 +153,26 @@ def run_command (argv):
     if not os.path.isdir (update_dir):
         os.mkdir (update_dir)
 
-    observer = Observer ()
-    observer.schedule(FileMonitor (), path=update_dir, recursive=True)
-    observer.start ()
+    wm = pyinotify.WatchManager ()
+    status.observer = pyinotify.Notifier (wm)
+    wm.add_watch (update_dir, pyinotify.IN_CLOSE_WRITE, proc_fun=FileMonitor ())
     signal.signal (signal.SIGTERM, shutdown)
 
-    while 1:
-        time.sleep (1)
-        print "."
+    obs = ObserverThread ()
+    obs.start ()
 
-        if stop:
+    # TODO status report should be done by SOAP in the future.
+    #      this is just a quick hack to use multithreading from the begining
+    while 1:
+        try:
+            time.sleep (1)
+        except KeyboardInterrupt:
+            status.stop = True
+
+        print status.msg
+
+        if status.stop:
             print "shutdown"
             sys.exit (0)
+
+    obs.join ()
