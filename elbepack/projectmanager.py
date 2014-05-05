aa59ea80563328bb8763d9e470ea16215e1cdca8
Stefan Gast <stefan.gast@linutronix.de>
2014-05-05 18:05:19 +0200

    Add a ProjectManager for open projects
    The new ProjectManager class will be used to track open projects and to
    map users to them. Each user can have one open project at a time,
    which is the user's active project. The class only allows one user per
    project at a time.
    
    Currently supported operations:
        * creating new projects
        * opening existing projects
        * closing the active project of a user
        * deleting projects
        * getting a file list of the current project
        * setting the XML file of the current project
        * putting the current project into the build queue of the
          AsyncBuilder
    
    Still need to implement project ownership and permission checks.
    
    Signed-off-by: Stefan Gast <stefan.gast@linutronix.de>

---
+++ elbepack/projectmanager.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+#
+# ELBE - Debian Based Embedded Rootfilesystem Builder
+# Copyright (C) 2014  Linutronix GmbH
+#
+# This file is part of ELBE.
+#
+# ELBE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ELBE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ELBE.  If not, see <http://www.gnu.org/licenses/>.
+
+
+from os import path
+from threading import Lock
+from uuid import uuid4
+from elbepack.db import ElbeDB, ElbeDBError
+from elbepack.asyncbuilder import AsyncBuilder
+
+class ProjectManagerError(Exception):
+    def __init__ (self, message):
+        Exception.__init__( self, message )
+
+class AlreadyOpen(ProjectManagerError):
+    def __init__ (self, builddir, username):
+        ProjectManagerError.__init__( self,
+                "project in %s is already opened by %s" % (builddir, username) )
+
+class NoOpenProject(ProjectManagerError):
+    def __init__ (self):
+        ProjectManagerError.__init__( self, "must open a project first" )
+
+class CurrentlyBuilt(ProjectManagerError):
+    def __init__ (self, message):
+        ProjectManagerError.__init__( self, message )
+
+
+class ProjectManager(object):
+    def __init__ (self, basepath):
+        self.basepath = basepath    # Base path for new projects
+        self.db = ElbeDB()          # Database of projects and users
+        self.builder = AsyncBuilder( self.db )
+        self.userid2project = {}    # (userid, ElbeProject) map of open projects
+        self.builddir2userid = {}   # (builddir, userid) map of open projects
+        self.lock = Lock()          # Lock protecting our data
+
+    def create_project (self, userid, xml_file):
+        subdir = str(uuid4())
+        builddir = path.join( self.basepath, subdir )
+
+        with self.lock:
+            # Try to close old project, if any
+            self._close_current_project( userid )
+
+            # TODO: Create with owner = userid
+            self.db.create_project( builddir )
+
+            try:
+                self.db.set_xml( builddir, xml_file )
+            except:
+                # Delete the project, if we cannot assign an XML file
+                self.db.del_project( builddir )
+                raise
+
+            # Open the new project
+            ep = self.db.load_project( builddir )
+            self.userid2project[ userid ] = ep
+            self.builddir2userid[ builddir ] = userid
+
+    def open_project (self, userid, builddir):
+        with self.lock:
+            if builddir in self.builddir2userid:
+                if self.builddir2userid[ builddir ] == userid:
+                    # Same project selected again by the same user, don't do
+                    # anything
+                    return
+                else:
+                    # Already opened by a different user
+                    raise AlreadyOpen( builddir,
+                            self.db.get_username(
+                                self.builddir2userid[ builddir ] ) )
+
+            # Try to close the old project of the user, if any
+            self._close_current_project( userid )
+
+            # Load project from the database
+            # TODO: Permission check
+            ep = self.db.load_project( builddir )
+
+            # Add project to our dictionaries
+            self.userid2project[ userid ] = ep
+            self.builddir2userid[ builddir ] = userid
+
+    def close_current_project (self, userid):
+        with self.lock:
+            self._close_current_project( userid )
+
+    def del_project (self, userid, builddir):
+        with self.lock:
+            # TODO: Permission check
+            # Does anyone have the project opened right now?
+            if builddir in self.builddir2userid:
+                if self.builddir2userid[ builddir ] == userid:
+                    # If the calling user has opened it, then close it and
+                    # proceed if closed sucessfully.
+                    self._close_current_project( userid )
+                else:
+                    # TODO: Admin should be allowed to delete projects
+                    # that are currently opened by other users
+                    raise AlreadyOpen( builddir,
+                            self.db.get_username(
+                                self.builddir2userid[ builddir ] ) )
+
+        self.db.del_project( builddir )
+
+    def get_current_project_data (self, userid):
+        with self.lock:
+            builddir = self._get_current_project( userid ).builddir
+            return self.db.get_project_data( builddir )
+
+    def get_current_project_files (self, userid):
+        with self.lock:
+            builddir = self._get_current_project( userid ).builddir
+            return self.db.get_files( builddir )
+
+    def set_current_project_xml (self, userid, xmlfile):
+        with self.lock:
+            ep = self._get_current_project( userid )
+            if self.db.is_build_in_progress( ep.builddir ):
+                raise CurrentlyBuilt(
+                        "cannot change XML file for project being currently built in %s" %
+                        ep.builddir )
+
+            self.db.set_xml( ep.builddir, xml_file )
+            ep.set_xml()    # Always use source.xml in the project directory
+
+    def build_current_project (self, userid):
+        with self.lock:
+            ep = self._get_current_project( userid )
+            if self.db.is_build_in_progress( ep.builddir ):
+                raise CurrentlyBuilt(
+                        "project %s is already being built" % ep.builddir )
+
+            self.builder.enqueue( ep )
+
+    def _get_current_project (self, userid):
+        # Must be called with self.lock held
+        if not userid in self.userid2project:
+            raise NoOpenProject()
+
+        return self.userid2project[ userid ]
+
+    def _close_current_project (self, userid):
+        # Must be called with self.lock held
+
+        if userid in self.userid2project:
+            builddir = self.userid2project[ userid ].builddir
+            if self.db.is_build_in_progress( builddir ):
+                raise CurrentlyBuilt(
+                        "project in directory %s of user %s is currently being built and cannot be closed" %
+                        ( builddir, self.db.get_username( userid ) ) )
+
+            del self.builddir2userid[ builddir ]
+            del self.userid2project[ userid ]
