Linutronix/elbe

commit d47f6fb2d4271974980dea8a101424659f9d7321

fix syntax errors
remove ')' that wasn't opened

Signed-off-by: Manuel Traut <manut@linutronix.de>

elbepack/filesystem.py
@@ -268,7 +268,7 @@ class ChRootFilesystem(Filesystem):
             ui = "/usr/share/elbe/qemu-elbe/" + self.interpreter
             if not os.path.exists (ui):
                 ui = "/usr/bin/" + self.interpreter
-            os.system ('cp %s %s' % ui, self.fname( "usr/bin" )) )
+            os.system ('cp %s %s' % ui, self.fname( "usr/bin" ))
 
         self.mount()
         return self

elbepack/rfs.py
@@ -127,7 +127,7 @@ class BuildEnv ():
             if not os.path.exists (ui):
                 ui = "/usr/bin/" + self.xml.defs["userinterpr"]
 
-            self.log.do ('cp %s %s' % ui, self.rfs.fname( "usr/bin" )) )
+            self.log.do ('cp %s %s' % ui, self.rfs.fname( "usr/bin" ))
 
             self.log.chroot (self.rfs.path,
                              '/debootstrap/debootstrap --second-stage')

commit 629b863a5ce2fb61b7bee6d5f23a979867c83961

fix bracketing issue introduced in d47f6fb2d42
Signed-off-by: Manuel Traut <manut@linutronix.de>

elbepack/filesystem.py
@@ -284,7 +284,7 @@ class ChRootFilesystem(Filesystem):
             ui = "/usr/share/elbe/qemu-elbe/" + self.interpreter
             if not os.path.exists (ui):
                 ui = "/usr/bin/" + self.interpreter
-            os.system ('cp %s %s' % ui, self.fname( "usr/bin" ))
+            os.system ('cp %s %s' % (ui, self.fname( "usr/bin" )))
 
         self.mount()
         return self

elbepack/rfs.py
@@ -127,7 +127,7 @@ class BuildEnv ():
             if not os.path.exists (ui):
                 ui = "/usr/bin/" + self.xml.defs["userinterpr"]
 
-            self.log.do ('cp %s %s' % ui, self.rfs.fname( "usr/bin" ))
+            self.log.do ('cp %s %s' % (ui, self.rfs.fname( "usr/bin" )))
 
             self.log.chroot (self.rfs.path,
                              '/debootstrap/debootstrap --second-stage')