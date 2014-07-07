46aee20e34397fa95964fe0446223d8535b4e930
Torben Hohn <torbenh@linutronix.de>
2014-07-07 18:18:19 +0200

    hdimg: rename grubinstaller to grubinstaller2 and add grubinstaller1

--- elbepack/hdimg.py
+++ elbepack/hdimg.py
@@ -174,7 +174,7 @@ def size_to_int( size ):
     return int(s) * unit
 
 
-class grubinstaller( object ):
+class grubinstaller2( object ):
     def __init__( self, outf ):
         self.outf = outf
         self.root = None
@@ -235,6 +235,63 @@ class grubinstaller( object ):
 
             self.outf.do( 'umount /dev/poop1', allow_fail=True )
             self.outf.do( 'losetup -d /dev/poop1', allow_fail=True )
+
+class grubinstaller1( object ):
+    def __init__( self, outf ):
+        self.outf = outf
+        self.root = None
+        self.boot = None
+
+    def set_boot_entry( self, entry ):
+        print "setting boot entry"
+        self.boot = entry
+
+    def set_root_entry( self, entry ):
+        self.root = entry
+
+    def install( self, target ):
+        if not self.root:
+            return
+
+        imagemnt = os.path.join(target, "imagemnt" )
+        try:
+            self.outf.do( 'cp -a /dev/loop0 /dev/poop0' )
+
+            self.outf.do( 'losetup /dev/poop0 "%s"' % self.root.filename )
+            self.outf.do( 'kpartx -as /dev/poop0' );
+            self.outf.do( 'mount /dev/mapper/poop0p%d %s' % (self.root.partnum,imagemnt))
+
+            if self.boot:
+                self.outf.do( 'mount /dev/mapper/poop0p%d %s' % (self.boot.partnum,os.path.join( imagemnt, "boot" ) ) )
+
+
+            self.outf.do( "mount --bind /dev %s" % os.path.join( imagemnt, "dev" ) )
+            self.outf.do( "mount --bind /proc %s" % os.path.join( imagemnt, "proc" ) )
+            self.outf.do( "mount --bind /sys %s" % os.path.join( imagemnt, "sys" ) )
+
+            self.outf.do( 'mkdir -p "%s"' % os.path.join( imagemnt, "boot/grub" ))
+
+            devmap = open( os.path.join( imagemnt, "boot/grub/device.map" ), "w" )
+            devmap.write( "(hd0) /dev/poop0\n" )
+            devmap.close()
+
+            self.outf.do( "chroot %s  update-grub"  % imagemnt )
+
+            self.outf.do( "chroot %s grub-install --no-floppy /dev/poop0" % (imagemnt))
+
+        finally:
+            self.outf.do( "rm %s" % os.path.join( imagemnt, "boot/grub/device.map" ), allow_fail=True )
+            self.outf.do( "umount %s" % os.path.join( imagemnt, "dev" ), allow_fail=True )
+            self.outf.do( "umount %s" % os.path.join( imagemnt, "proc" ), allow_fail=True )
+            self.outf.do( "umount %s" % os.path.join( imagemnt, "sys" ), allow_fail=True )
+
+            if self.boot:
+                self.outf.do( 'umount /dev/mapper/poop0p%d' % self.boot.partnum, allow_fail=True )
+
+            self.outf.do( 'umount /dev/mapper/poop0p%d' % self.root.partnum, allow_fail=True )
+
+            self.outf.do( 'kpartx -d /dev/poop0', allow_fail=True );
+            self.outf.do( "losetup -d /dev/poop0", allow_fail=True )
 
 class simple_fstype(object):
     def __init__(self, typ):
@@ -306,6 +363,7 @@ def do_image_hd( outf, hd, fslabel, target, grub_version ):
         entry.offset = current_sector*sector_size
         entry.size   = sz * sector_size
         entry.filename = imagename
+        entry.partnum = partition_number
         if hd.tag == "gpthd":
             entry.number = "gpt%d" % partition_number
         else:
