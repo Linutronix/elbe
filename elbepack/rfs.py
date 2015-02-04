1ba8dd5af4071bf5a885c3d1a387fae553646587
Manuel Traut <manut@linutronix.de>
2015-02-04 11:08:08 +0100

    jessie/systemd: allow_fail if console link already exists
    otherwise incremental jessie builds are broken
    
    Signed-off-by: Manuel Traut <manut@linutronix.de>

--- elbepack/rfs.py
+++ elbepack/rfs.py
@@ -221,6 +221,6 @@ class BuildEnv ():
             if (self.xml.text( "project/suite" ) == "wheezy") or (self.xml.text( "project/suite" ) == "squeeze"):
                 self.log.chroot(self.rfs.path, """/bin/sh -c 'echo "T0:23:respawn:/sbin/getty -L %s %s vt100" >> /etc/inittab'""" % (serial_con, serial_baud) )
             else:
-                self.log.chroot(self.rfs.path, """/bin/sh -c 'ln -s /lib/systemd/system/serial-getty@.service /etc/systemd/system/getty.target.wants/serial-getty@%s.service'""" % serial_con )
+                self.log.chroot(self.rfs.path, """/bin/sh -c 'ln -s /lib/systemd/system/serial-getty@.service /etc/systemd/system/getty.target.wants/serial-getty@%s.service'""" % serial_con, allow_fail=True )
         else:
             self.log.printo("parsing console tag failed, needs to be of '/dev/ttyS0,115200' format.")
