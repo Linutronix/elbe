081d5e833c82e1819366c888866cbd4335933578
Benedikt Spranger <b.spranger@linutronix.de>
2015-03-12 13:36:12 +0100

    sysvinit/systemd: Simplify inittab resp. service file handling
    The former test based on suite is not sufficient. Through the packet
    list it is rather simple to install systemd or sysvinit. Simplyfy the
    tweaks by testing for the file to tweak.
    
    Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>
    Signed-off-by: Manuel Traut <manut@linutronix.de>

--- elbepack/rfs.py
+++ elbepack/rfs.py
@@ -218,9 +218,7 @@ class BuildEnv ():
 
         serial_con, serial_baud = self.xml.text( "target/console" ).split(',')
         if serial_baud:
-            if (self.xml.text( "project/suite" ) == "wheezy") or (self.xml.text( "project/suite" ) == "squeeze"):
-                self.log.chroot(self.rfs.path, """/bin/sh -c 'echo "T0:23:respawn:/sbin/getty -L %s %s vt100" >> /etc/inittab'""" % (serial_con, serial_baud) )
-            else:
-                self.log.chroot(self.rfs.path, """/bin/sh -c 'ln -s /lib/systemd/system/serial-getty@.service /etc/systemd/system/getty.target.wants/serial-getty@%s.service'""" % serial_con, allow_fail=True )
+            self.log.chroot(self.rfs.path, """/bin/sh -c '[ -f /etc/inittab ] && echo "T0:23:respawn:/sbin/getty -L %s %s vt100" >> /etc/inittab'""" % (serial_con, serial_baud), allow_fail=True )
+            self.log.chroot(self.rfs.path, """/bin/sh -c '[ -f /lib/systemd/system/serial-getty@.service ] && ln -s /lib/systemd/system/serial-getty@.service /etc/systemd/system/getty.target.wants/serial-getty@%s.service'""" % serial_con, allow_fail=True )
         else:
             self.log.printo("parsing console tag failed, needs to be of '/dev/ttyS0,115200' format.")
