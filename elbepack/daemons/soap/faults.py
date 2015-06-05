3c39dd6caba8d917d771cb76ccdca4971afdd4b0
Torben Hohn <torbenh@linutronix.de>
2015-06-05 15:15:58 +0200

    daemons soap faults: Add decorator which wraps Exceptions into SoapFaults

--- elbepack/daemons/soap/faults.py
+++ elbepack/daemons/soap/faults.py
@@ -17,6 +17,15 @@
 # along with ELBE.  If not, see <http://www.gnu.org/licenses/>.
 
 from soaplib.serializers.primitive import Fault
+from traceback import format_exc
+
+from functools import wraps
+
+# Import the Errors we try to catch wirh the
+# soap_faults decorator
+from elbepack.projectmanager import ProjectManagerError, InvalidState
+from elbepack.elbexml import ValidationError
+from elbepack.db import ElbeDBError, InvalidLogin
 
 
 class SoapElbeDBError( Fault ):
@@ -46,4 +55,132 @@ class SoapElbeValidationError( Fault ):
 class SoapElbeInvalidState( Fault ):
     def __init__(self):
         Fault.__init__(self, faultcode="ElbeInvalidState", faultstring="Project is Busy ! Operation Invalid")
+
+
+def soap_faults(func):
+    """ decorator, which wraps Exceptions to the proper
+        Soap Faults, and raises these.
+    """
+    if func.func_code.co_argcount == 1:
+        @wraps(func)
+        def wrapped(self):
+            try:
+                return func(self)
+            except ProjectManagerError as e:
+                raise SoapElbeProjectError (str (e))
+            except InvalidState as e:
+                raise SoapElbeInvalidState()
+            except ElbeDBError as e:
+                raise SoapElbeDBError (str (e))
+            except OSError as e:
+                raise SoapElbepProjectError ("OSError: " + str (e))
+            except ValidationError as e:
+                raise SoapElbeValidationError ()
+            except InvalidLogin:
+                raise SoapElbeNotAuthorized()
+            except Exception as e:
+                raise SoapElbeProjectError (format_exc ())
+        return wrapped
+    if func.func_code.co_argcount == 2:
+        @wraps (func)
+        def wrapped (self, arg1):
+            try:
+                return func (self,arg1)
+            except ProjectManagerError as e:
+                raise SoapElbeProjectError (str (e))
+            except InvalidState as e:
+                raise SoapElbeInvalidState ()
+            except ElbeDBError as e:
+                raise SoapElbeDBError (str (e))
+            except OSError as e:
+                raise SoapElbepProjectError ("OSError: " + str (e))
+            except ValidationError as e:
+                raise SoapElbeValidationError ()
+            except InvalidLogin:
+                raise SoapElbeNotAuthorized()
+            except Exception as e:
+                raise SoapElbeProjectError (format_exc ())
+        return wrapped
+    if func.func_code.co_argcount == 3:
+        @wraps (func)
+        def wrapped (self, arg1, arg2):
+            try:
+                return func (self,arg1,arg2)
+            except ProjectManagerError as e:
+                raise SoapElbeProjectError (str (e))
+            except InvalidState as e:
+                raise SoapElbeInvalidState ()
+            except ElbeDBError as e:
+                raise SoapElbeDBError (str (e))
+            except OSError as e:
+                raise SoapElbepProjectError ("OSError: " + str (e))
+            except ValidationError as e:
+                raise SoapElbeValidationError ()
+            except InvalidLogin:
+                raise SoapElbeNotAuthorized()
+            except Exception as e:
+                raise SoapElbeProjectError (format_exc ())
+        return wrapped
+    if func.func_code.co_argcount == 4:
+        @wraps (func)
+        def wrapped (self, arg1, arg2, arg3):
+            try:
+                return func (self,arg1,arg2, arg3)
+            except ProjectManagerError as e:
+                raise SoapElbeProjectError (str (e))
+            except InvalidState as e:
+                raise SoapElbeInvalidState ()
+            except ElbeDBError as e:
+                raise SoapElbeDBError (str (e))
+            except OSError as e:
+                raise SoapElbepProjectError ("OSError: " + str (e))
+            except ValidationError as e:
+                raise SoapElbeValidationError ()
+            except InvalidLogin:
+                raise SoapElbeNotAuthorized()
+            except Exception as e:
+                raise SoapElbeProjectError (format_exc ())
+        return wrapped
+    if func.func_code.co_argcount == 5:
+        @wraps (func)
+        def wrapped (self, arg1, arg2, arg3, arg4):
+            try:
+                return func (self,arg1,arg2, arg3, arg4)
+            except ProjectManagerError as e:
+                raise SoapElbeProjectError (str (e))
+            except InvalidState as e:
+                raise SoapElbeInvalidState ()
+            except ElbeDBError as e:
+                raise SoapElbeDBError (str (e))
+            except OSError as e:
+                raise SoapElbepProjectError ("OSError: " + str (e))
+            except ValidationError as e:
+                raise SoapElbeValidationError ()
+            except InvalidLogin:
+                raise SoapElbeNotAuthorized()
+            except Exception as e:
+                raise SoapElbeProjectError (format_exc ())
+        return wrapped
+    if func.func_code.co_argcount == 6:
+        @wraps (func)
+        def wrapped (self, arg1, arg2, arg3, arg4, arg5):
+            try:
+                return func (self,arg1,arg2, arg3, arg4, arg5)
+            except ProjectManagerError as e:
+                raise SoapElbeProjectError (str (e))
+            except InvalidState as e:
+                raise SoapElbeInvalidState ()
+            except ElbeDBError as e:
+                raise SoapElbeDBError (str (e))
+            except OSError as e:
+                raise SoapElbepProjectError ("OSError: " + str (e))
+            except ValidationError as e:
+                raise SoapElbeValidationError ()
+            except InvalidLogin:
+                raise SoapElbeNotAuthorized()
+            except Exception as e:
+                raise SoapElbeProjectError (format_exc ())
+        return wrapped
+    else:
+        raise Exception( "arg count %d not implemented" % func.func_code.co_argcount )
 
