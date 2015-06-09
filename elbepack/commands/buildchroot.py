bfc1a0d7b8e866f221c2be9967a70a6c22b32130
Torben Hohn <torbenh@linutronix.de>
2015-06-09 17:23:27 +0200

    commands buildchroot: reorder options in order of importance

--- elbepack/commands/buildchroot.py
+++ elbepack/commands/buildchroot.py
@@ -31,39 +31,52 @@ from sqlalchemy.exc import OperationalError
 
 def run_command( argv ):
     oparser = OptionParser(usage="usage: %prog buildchroot [options] <xmlfile>")
+
     oparser.add_option( "-t", "--target", dest="target",
                         help="directoryname of target" )
+
     oparser.add_option( "-o", "--output", dest="output",
                         help="name of logfile" )
+
     oparser.add_option( "-n", "--name", dest="name",
                         help="name of the project (included in the report)" )
-    oparser.add_option( "--skip-validation", action="store_true",
-                        dest="skip_validation", default=False,
-                        help="Skip xml schema validation" )
-    oparser.add_option( "--skip-debootstrap", action="store_true",
-                        dest="skip_debootstrap", default=False,
-                        help="Skip debootstrap" )
-    oparser.add_option( "--skip-pkglist", action="store_true",
-                        dest="skip_pkglist", default=False,
-                        help="ignore changes of the package list" )
-    oparser.add_option( "--skip-cdrom", action="store_true",
-                        dest="skip_cdrom", default=False,
-                        help="(now obsolete) Skip cdrom iso generation" )
+
     oparser.add_option( "--build-bin", action="store_true",
                         dest="build_bin", default=False,
                         help="Build Binary Repository CDROM, for exact Reproduction" )
+
     oparser.add_option( "--build-sources", action="store_true",
                         dest="build_sources", default=False,
                         help="Build Source CD" )
-    oparser.add_option( "--cdrom-size", action="store",
-                        dest="cdrom_size", default=CDROM_SIZE, help="ISO CD size in MB" )
+
+    oparser.add_option( "--proxy", dest="proxy",
+                        help="Override the http proxy" )
+
     oparser.add_option( "--debug", action="store_true", dest="debug",
                         default=False,
                         help="Enable various features to debug the build" )
+
     oparser.add_option( "--buildtype", dest="buildtype",
                         help="Override the buildtype" )
-    oparser.add_option( "--proxy", dest="proxy",
-                        help="Override the http proxy" )
+
+    oparser.add_option( "--cdrom-size", action="store",
+                        dest="cdrom_size", default=CDROM_SIZE, help="ISO CD size in MB" )
+
+    oparser.add_option( "--skip-validation", action="store_true",
+                        dest="skip_validation", default=False,
+                        help="Skip xml schema validation" )
+
+    oparser.add_option( "--skip-debootstrap", action="store_true",
+                        dest="skip_debootstrap", default=False,
+                        help="Skip debootstrap" )
+
+    oparser.add_option( "--skip-pkglist", action="store_true",
+                        dest="skip_pkglist", default=False,
+                        help="ignore changes of the package list" )
+
+    oparser.add_option( "--skip-cdrom", action="store_true",
+                        dest="skip_cdrom", default=False,
+                        help="(now obsolete) Skip cdrom iso generation" )
 
     (opt,args) = oparser.parse_args(argv)
 
