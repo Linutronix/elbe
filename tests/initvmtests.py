43e5ad975451739ccd1324e92a20aa2ae9e9c5cf
Torben Hohn <torbenh@linutronix.de>
2015-08-17 14:35:13 +0200

    tests: better initvmtests

--- tests/initvmtests.py
+++ tests/initvmtests.py
@@ -3,6 +3,7 @@ import elbepack
 import os
 import sys
 import shutil
+from time import sleep
 
 from elbepack.directories import init_directories
 from elbepack.shellhelper import system
@@ -10,24 +11,32 @@ from elbepack.shellhelper import system
 exe_path = None
 
 def setup():
-    global exe_path
-    # Properly initialise directories plug
-    mod_dir = os.path.dirname (__file__)
-    main_dir, _  = os.path.split( mod_dir )
-    exe_path  = os.path.join( main_dir, "elbe" )
+    try:
+        global exe_path
+        # Properly initialise directories plug
+        mod_dir = os.path.dirname (__file__)
+        main_dir, _  = os.path.split( mod_dir )
+        exe_path  = os.path.join( main_dir, "elbe" )
 
-    init_directories (exe_path)
+        init_directories (exe_path)
 
-    tmpdir = os.getenv( "ELBE_TEST_DIR" )
-    assert tmpdir is not None
+        tmpdir = os.getenv( "ELBE_TEST_DIR" )
+        assert tmpdir is not None
 
-    dname = os.path.join(tmpdir, "initvm_test")
+        dname = os.path.join(tmpdir, "initvm_test")
 
-    from elbepack.directories import examples_dir
-    xml_name = os.path.join (examples_dir, "elbe-init-with-ssh.xml")
-    system ('%s initvm create --directory "%s" %s' % (exe_path, dname, xml_name))
+        from elbepack.directories import examples_dir
+        xml_name = os.path.join (examples_dir, "elbe-init-with-ssh.xml")
+        system ('%s initvm create --devel --directory "%s" %s' % (exe_path, dname, xml_name))
+    except:
+        teardown ()
+        raise
+        
 
 def teardown():
+
+    system ("%s control shutdown_initvm" % (exe_path), allow_fail=True)
+    sleep (10)
     tmpdir = os.getenv( "ELBE_TEST_DIR" )
     assert tmpdir is not None
 
