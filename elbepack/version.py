26965c2543ca1133e93f5cef22c9fc07508a3d8d
Manuel Traut <manut@linutronix.de>
2015-08-28 15:17:54 +0200

    version bump 0.9.6.4
    Signed-off-by: Manuel Traut <manut@linutronix.de>

--- elbepack/version.py
+++ elbepack/version.py
@@ -20,7 +20,7 @@
 
 from elbepack.directories import pack_dir
 
-elbe_version = "0.9.6.3"
+elbe_version = "0.9.6.4"
 
 if pack_dir == '/usr/lib/python2.7/dist-packages/elbepack':
     is_devel = False
