d8449645374cf4cd5848f1084a0246a55a67ebbd
Torben Hohn <torben.hohn@linutronix.de>
2016-08-25 16:31:48 +0200

    config: add simple implementation of config object
    only sets cfg['soapport'] and cfg['pbuilder_jobs']
    via env vars.
    
    ELBE_SOAPPORT
    ELBE_PBUILDER_JOBS

---
+++ elbepack/config.py
@@ -0,0 +1,33 @@
+#
+# ELBE - Debian Based Embedded Rootfilesystem Builder
+# Copyright (C) 2014  Linutronix GmbH
+#
+# This file is part of ELBE.
+#
+# ELBE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ELBE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ELBE.  If not, see <http://www.gnu.org/licenses/>.
+
+class Config(dict):
+    def __init__(self):
+        self['soapport'] = "7587"
+        self['pbuilder_jobs'] = "auto"
+
+        if os.environ.has_key('ELBE_SOAPPORT'):
+            self['soapport'] = os.environ['ELBE_SOAPPORT']
+
+        if os.environ.has_key('ELBE_PBUILDER_JOBS'):
+            self['pbuilder_jobs'] = os.environ['ELBE_PBUILDER_JOBS']
+
+
+cfg = Config()
+
