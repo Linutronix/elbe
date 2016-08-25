6fbf9e6b187cd3378e0f7f9d0dc68736630bade4
Torben Hohn <torben.hohn@linutronix.de>
2016-08-25 17:43:52 +0200

    config: add missing "import os"

--- elbepack/config.py
+++ elbepack/config.py
@@ -17,6 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with ELBE.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+
 class Config(dict):
     def __init__(self):
         self['soapport'] = "7587"
