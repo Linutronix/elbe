823e9077463fa02a4588b003db2ee22efaa0e79f
Manuel Traut <manut@linutronix.de>
2017-02-08 06:12:56 +0100

    version bump: v1.9.16
    Signed-off-by: Manuel Traut <manut@linutronix.de>

--- elbepack/version.py
+++ elbepack/version.py
@@ -21,7 +21,7 @@
 from elbepack.directories import pack_dir
 from platform import linux_distribution
 
-elbe_version = "1.9.15"
+elbe_version = "1.9.16"
 running_os = linux_distribution ()
 
 if pack_dir == '/usr/lib/python2.7/dist-packages/elbepack':
