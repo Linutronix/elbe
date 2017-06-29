a46fb85e68550126443d8bf68c872ffd725cc8cc
Manuel Traut <manut@linutronix.de>
2017-06-29 14:52:55 +0200

    debianize: fix fallout from unused import removal
    few days ago, i removed all unused imports. This one was
    wrongly detected by pylint..
    
    Signed-off-by: Manuel Traut <manut@linutronix.de>

--- elbepack/debianize/debianize.py
+++ elbepack/debianize/debianize.py
@@ -20,6 +20,14 @@
 
 from npyscreen import NPSAppManaged
 
+# don't remove these imports; even if pylint, etc believes they are unused
+from elbepack.debianize.kernel import Kernel
+from elbepack.debianize.uboot  import UBoot
+from elbepack.debianize.barebox import BareBox
+
+from elbepack.debianize.base import DebianizeBase
+
+
 class Debianize (NPSAppManaged):
     def __init__ (self, debianizer):
         self.debianizer = debianizer
