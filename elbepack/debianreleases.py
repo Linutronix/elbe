6c2bd59df6887c5ea941c097c224643d9c8fc08f
Manuel Traut <manut@linutronix.de>
2017-08-07 08:56:54 +0200

    debianreleases: make codenames / releases uniq
    otherwise codename2suite returns a keyerror
    
    Signed-off-by: Manuel Traut <manut@linutronix.de>

--- elbepack/debianreleases.py
+++ elbepack/debianreleases.py
@@ -16,8 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with ELBE.  If not, see <http://www.gnu.org/licenses/>.
 
-suite2codename = { 'oldstable': 'squeeze',
-                   'oldstable': 'wheezy',
+suite2codename = { 'oldoldoldstable': 'squeeze',
+                   'oldoldstable': 'wheezy',
                    'oldstable': 'jessie',
                    'stable': 'stretch',
                    'testing': 'buster',
