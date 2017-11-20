af47ee76d2f767b3441d5875f4f3575da66949b8
John Ogness <john.ogness@linutronix.de>
2017-11-20 15:05:57 +0100

    initvm/create: fix source.xml access/usage
    The source.xml file is stored in the tmp directory, not in the
    local working directory.
    
    The set_xml command expects the project ID and XML file as
    separate parameters. Remove coupling quotes.
    
    Signed-off-by: John Ogness <john.ogness@linutronix.de>
    Signed-off-by: Manuel Traut <manut@linutronix.de>

--- elbepack/initvmaction.py
+++ elbepack/initvmaction.py
@@ -332,7 +332,7 @@ class CreateAction(InitVMAction):
                 exampl = args[0]
             elif cdrom is not None:
                 ret, prjdir, err = command_out_stderr ('%s control create_project' % (elbe_exe))
-                exampl = 'source.xml'
+                exampl = tmp.fname ('source.xml')
             else:
                 ret, prjdir, err = command_out_stderr ('%s control create_project' % (elbe_exe))
 
@@ -344,7 +344,7 @@ class CreateAction(InitVMAction):
 
             prjdir = prjdir.strip()
 
-            ret, msg, err = command_out_stderr ('%s control set_xml "%s %s"' % (elbe_exe, prjdir, exampl))
+            ret, msg, err = command_out_stderr ('%s control set_xml %s %s' % (elbe_exe, prjdir, exampl))
             if ret != 0:
                 print ("elbe control set_xml failed.", file=sys.stderr)
                 print (err, file=sys.stderr)
