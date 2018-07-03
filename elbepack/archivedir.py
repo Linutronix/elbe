Linutronix/elbe

commit 1fb72f395a64d373294986ddea1683f727613468

Split out change archive functionality
The new <archivedir> feature has common task equal to chg_archive.
This preperatory patch split out the change archive functionality from
the chg_archive command into a module.

Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>
Signed-off-by: Manuel Traut <manut@linutronix.de>

elbepack/archivedir.py
@@ -1,0 +1,42 @@
+# ELBE - Debian Based Embedded Rootfilesystem Builder
+# Copyright (c) 2018 Benedikt Spranger <b.spranger@linutronix.de>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import os
+
+from base64 import standard_b64encode
+from elbepack.treeutils import etree
+
+def enbase(fname):
+    infile = file(fname, "r")
+    s = infile.read()
+    enc = standard_b64encode(s)
+
+    splited = ""
+    i = 0
+    l_enc = len(enc)
+    while i < l_enc:
+        splited += (enc[i:i + 60] + "\n")
+        i += 60
+
+    return splited
+
+def chg_archive(xml, path, keep):
+    if os.path.isdir(path):
+        archive = '.archive.tbz'
+        if keep:
+            cmd = 'tar cfj .archive.tbz -C '
+        else:
+            cmd = 'tar cjf .archive.tbz --owner=root --group=root -C '
+        cmd += path + ' .'
+        os.system(cmd)
+    else:
+        archive = path
+        arch = xml.ensure_child("archive")
+        arch.set_text(enbase(archive))
+
+    if os.path.isdir(path):
+        os.remove(archive)
+
+    return xml

debian/python-elbe-common.install
@@ -8,6 +8,7 @@
 ./usr/lib/python2.*/*-packages/elbepack/__init__.py
 ./usr/lib/python2.*/*-packages/elbepack/commands/__init__.py
 ./usr/lib/python2.*/*-packages/elbepack/aptprogress.py
+./usr/lib/python2.*/*-packages/elbepack/archivedir.py
 ./usr/lib/python2.*/*-packages/elbepack/config.py
 ./usr/lib/python2.*/*-packages/elbepack/default-preseed.xml
 ./usr/lib/python2.*/*-packages/elbepack/directories.py

debian/python3-elbe-common.install
@@ -8,6 +8,7 @@
 ./usr/lib/python3.*/*-packages/elbepack/__init__.py
 ./usr/lib/python3.*/*-packages/elbepack/commands/__init__.py
 ./usr/lib/python3.*/*-packages/elbepack/aptprogress.py
+./usr/lib/python3.*/*-packages/elbepack/archivedir.py
 ./usr/lib/python3.*/*-packages/elbepack/config.py
 ./usr/lib/python3.*/*-packages/elbepack/default-preseed.xml
 ./usr/lib/python3.*/*-packages/elbepack/directories.py

elbepack/commands/chg_archive.py
@@ -8,26 +8,9 @@ from __future__ import print_function
 import sys
 import os
 
-from base64 import standard_b64encode
-
+from elbepack.archivedir import chg_archive
 from elbepack.treeutils import etree
 from optparse import OptionParser
-
-
-def enbase(fname):
-    infile = file(fname, "r")
-    s = infile.read()
-    enc = standard_b64encode(s)
-
-    splited = ""
-    i = 0
-    l_enc = len(enc)
-    while i < l_enc:
-        splited += (enc[i:i + 60] + "\n")
-        i += 60
-
-    return splited
-
 
 def run_command(argv):
 
@@ -55,20 +38,8 @@ def run_command(argv):
         print("Error reading xml file!")
         sys.exit(20)
 
-    if os.path.isdir(args[1]):
-        archive = '.archive.tbz'
-        if opt.keep_attributes:
-            cmd = 'tar cfj .archive.tbz -C '
-        else:
-            cmd = 'tar cjf .archive.tbz --owner=root --group=root -C '
-        cmd += args[1] + ' .'
-        os.system(cmd)
-    else:
-        archive = args[1]
-
     try:
-        arch = xml.ensure_child("archive")
-        arch.set_text(enbase(archive))
+        xml = chg_archive(xml, args[1], opt.keep_attributes)
     except BaseException:
         print("Error reading archive")
         sys.exit(20)
@@ -78,6 +49,3 @@ def run_command(argv):
     except BaseException:
         print("Unable to write new xml file")
         sys.exit(20)
-
-    if os.path.isdir(args[1]):
-        os.remove(archive)

commit d1a88e04a8e06b5912a4f975cf48b62bdc588c89

Fix indention error in elbepack/archivedir.py
commit 1fb72f395a64 ("Split out change archive functionality") broke
"elbe chg_archive dir" silently due to an indention error.

Fix the indention to add the new archive to the recipe in both cases:
"elbe chg_archive dir" and "elbe chg_archive archive.tbz"

Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>
Signed-off-by: Manuel Traut <manut@linutronix.de>

elbepack/archivedir.py
@@ -33,8 +33,9 @@ def chg_archive(xml, path, keep):
         os.system(cmd)
     else:
         archive = path
-        arch = xml.ensure_child("archive")
-        arch.set_text(enbase(archive))
+
+    arch = xml.ensure_child("archive")
+    arch.set_text(enbase(archive))
 
     if os.path.isdir(path):
         os.remove(archive)