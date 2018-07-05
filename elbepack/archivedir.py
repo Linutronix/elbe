216884b9b80bd7b34853e844b60ed7f05ea0cac5
Benedikt Spranger <b.spranger@linutronix.de>
2018-07-05 10:44:35 +0200

    Use tar append to create archives
    The new element <archivedir> which will be introduced in the next patch
    may occur multiple times to generate an archive from a set of different
    directories.
    
    The existing chg_archive implementation uses the "-j" flag while calling
    tar to compress the archive. Compressed archives can not be extended, but
    the upcoming <archivedir> feature depends on it.
    
    Generating the 'tar' archive needs to be split up into two parts:
    
    a) generating the archive (may be called multiple times; implented in
       the collect function)
    
    b) compressing the archive is not done with 'tar -j' but in python during
       enbasing the .tar archive.
    
    Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>
    Signed-off-by: Manuel Traut <manut@linutronix.de>
    Reviewed-by: Manuel Traut <manut@linutronix.de>

--- elbepack/archivedir.py
+++ elbepack/archivedir.py
@@ -6,15 +6,18 @@
 import os
 
 from base64 import standard_b64encode
+from bz2 import compress as bz2compress
 from subprocess import check_call
 
 from elbepack.treeutils import etree
 
-def enbase(fname):
+def enbase(fname, compress=True):
     infile = file(fname, "r")
     s = infile.read()
-    enc = standard_b64encode(s)
+    if compress:
+        s = bz2compress(s)
 
+    enc = standard_b64encode(s)
     splited = ""
     i = 0
     l_enc = len(enc)
@@ -24,20 +27,28 @@ def enbase(fname):
 
     return splited
 
+def collect(tararchive, path, keep):
+    if keep:
+        cmd = 'tar rf ' + tararchive + ' -C '
+    else:
+        cmd = 'tar rf ' + tararchive + ' --owner=root --group=root -C '
+    cmd += path + ' .'
+    check_call(cmd, shell=True)
+
 def chg_archive(xml, path, keep):
     if os.path.isdir(path):
-        archive = '.archive.tbz'
-        if keep:
-            cmd = 'tar cfj .archive.tbz -C '
-        else:
-            cmd = 'tar cjf .archive.tbz --owner=root --group=root -C '
-        cmd += path + ' .'
-        check_call(cmd, shell=True)
+        archive = '.archive.tar'
+        if os.path.exists(archive):
+            os.remove(archive)
+
+        collect(archive, path, keep)
+        compress = True
     else:
         archive = path
+        compress = False
 
     arch = xml.ensure_child("archive")
-    arch.set_text(enbase(archive))
+    arch.set_text(enbase(archive, compress))
 
     if os.path.isdir(path):
         os.remove(archive)
