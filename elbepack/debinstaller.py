0812cb0972b41d11c2a622d03f43d997168344b9
Bastian Germann <bage@linutronix.de>
2019-01-23 10:53:35 +0100

    Use absolute_import for ambiguous gpg imports
    Signed-off-by: Bastian Germann <bage@linutronix.de>

--- elbepack/debinstaller.py
+++ elbepack/debinstaller.py
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from __future__ import print_function
+from __future__ import absolute_import, print_function
 
 import sys
 import os
--- elbepack/finetuning.py
+++ elbepack/finetuning.py
@@ -6,7 +6,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from __future__ import print_function
+from __future__ import absolute_import, print_function
 
 import os
 
