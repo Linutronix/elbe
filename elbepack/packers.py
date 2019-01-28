de50393773e0a9ffcd578e6002347bc9ae8e6fe2
Torben Hohn <torben.hohn@linutronix.de>
2019-01-28 10:01:12 +0100

    packers: add Packer class implementation
    using tar as a packer can not be modelled using the tuples
    approach.
    
    Implement a Baseclass and a dict mapping packernames to
    Packer objects. Also implement NoPacker class, which does nothing
    and returns the fname right away.
    
    Signed-off-by: Torben Hohn <torben.hohn@linutronix.de>
    Signed-off-by: Manuel Traut <manut@linutronix.de>
      [added rm -f of origfile as suggested by John, and accepted by Torben]
    
    Reviewed-by: John Ogness <john.ogness@linutronix.de>

---
+++ elbepack/packers.py
@@ -0,0 +1,78 @@
+# ELBE - Debian Based Embedded Rootfilesystem Builder
+# Copyright (c) 2019 Torben Hohn <torben.hohn@linutronix.de>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import os
+from elbepack.shellhelper import CommandError
+
+
+class Packer(object):
+    # pylint: disable=too-few-public-methods
+    def pack_file(self, _log, _builddir, _fname):
+        raise NotImplementedError('abstract method called')
+
+
+class NoPacker(Packer):
+    # pylint: disable=too-few-public-methods
+    def pack_file(self, _log, _builddir, fname):
+        return fname
+
+
+class InPlacePacker(Packer):
+    # pylint: disable=too-few-public-methods
+    def __init__(self, cmd, suffix):
+
+        self.cmd = cmd
+        self.suffix = suffix
+
+    def pack_file(self, log, builddir, fname):
+        try:
+            fpath = os.path.join(builddir, fname)
+            log.do('%s "%s"' % (self.cmd, fpath))
+        except CommandError:
+            # in case of an error, we just return None
+            # which means, that the orig file does not
+            # exist anymore
+            return None
+
+        return fname + self.suffix
+
+
+class TarArchiver(Packer):
+    # pylint: disable=too-few-public-methods
+    def __init__(self, flag, suffix):
+        self.flag = flag
+        self.suffix = suffix
+
+    def pack_file(self, log, builddir, fname):
+        try:
+            fpath = os.path.join(builddir, fname)
+            dirname = os.path.dirname(fpath)
+            basename = os.path.basename(fpath)
+            archname = fpath + self.suffix
+            log.do('tar cv%sf "%s" --sparse -C "%s" "%s"' % (self.flag,
+                                                             archname,
+                                                             dirname,
+                                                             basename))
+            log.do('rm -f "%s"' % fpath)
+        except CommandError:
+            # in case of an error, we just return None
+            # which means, that the orig file does not
+            # exist anymore.
+            #
+            # Even if it actually exists, it might be
+            # much to big to download it and remove
+            # the sparsity.
+            return None
+
+        return fname + self.suffix
+
+
+packers = {'none': None,
+           'gzip': InPlacePacker('gzip -f', '.gz'),
+           'tar':  TarArchiver('', '.tar'),
+           'tarxz': TarArchiver('J', '.tar.xz'),
+           'targz': TarArchiver('z', '.tar.gz')}
+
+default_packer = packers['targz']
