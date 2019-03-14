acf716909c87599bb70f849d18147dfe0d531597
Manuel Traut <manut@linutronix.de>
2019-03-14 15:03:14 +0100

    version bump elbe 3
    Signed-off-by: Manuel Traut <manut@linutronix.de>

--- elbepack/version.py
+++ elbepack/version.py
@@ -8,7 +8,7 @@ from platform import linux_distribution
 
 from elbepack.directories import pack_dir
 
-elbe_version = "2.9.18"
+elbe_version = "3"
 running_os = linux_distribution()
 
 elbe_initvm_packagelist = ['python-elbe-buildenv',
