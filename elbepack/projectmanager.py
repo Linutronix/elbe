Linutronix/elbe

commit a02dfd19b303620242d95810d6bbdc4bb2c86185

projectmanager: allow add_deb_package() to add already existing pkg
includedsc() and includedeb() will raise an Exception, when a pkg
is already in the repo. But we want to be able to add already existing
pkg to the repository, when they are rebuilt.

Use force=True and also determine pkg_name from the filename and
pass that also, because force=True needs the pkgname.

Signed-off-by: Torben Hohn <torben.hohn@linutronix.de>
Signed-off-by: Manuel Traut <manut@linutronix.de>
Reviewed-by: Manuel Traut <manut@linutronix.de>

elbepack/projectmanager.py
@@ -513,11 +513,14 @@ class ProjectManager(object):
         ep = self._get_current_project(userid)
 
         t = filename[-3:]  # filetype of uploaded file
+        pkg_name = filename.split('_')[0]
 
         if t == 'dsc':
-            ep.repo.includedsc(os.path.join(ep.builddir, filename))
+            ep.repo.includedsc(os.path.join(ep.builddir, filename),
+                               pkgname=pkg_name, force=True)
         elif t == 'deb':
-            ep.repo.includedeb(os.path.join(ep.builddir, filename))
+            ep.repo.includedeb(os.path.join(ep.builddir, filename),
+                               pkgname=pkg_name, force=True)
 
         ep.repo.finalize()
 

commit 88a9cdb95baa8d5cdad790152ccf5ea046f7bec0

projectmanager: fix upload of dsc files
Currently it's not possible to upload a dsc file to the project repository:

  $ elbe prjrepo upload_pkg <project> <dsc_file>
  [...]
  Traceback (most recent call last):
    File "/usr/lib/python2.7/dist-packages/elbepack/daemons/soap/faults.py", line 139, in wrapped
      return func(self, arg1, arg2, arg3)
    File "/usr/lib/python2.7/dist-packages/elbepack/daemons/soap/esoap.py", line 424, in include_package
      self.app.pm.add_deb_package(uid, filename)
    File "/usr/lib/python2.7/dist-packages/elbepack/projectmanager.py", line 506, in add_deb_package
      pkgname=pkg_name, force=True)
  TypeError: includedsc() got an unexpected keyword argument 'pkgname'

includedsc() doesn't have a pkgname parameter. Remove it.

Fixes: a02dfd19b303 ("projectmanager: allow add_deb_package() to add already existing pkg")
Signed-off-by: Kurt Kanzenbach <kurt@linutronix.de>
Signed-off-by: Manuel Traut <manut@linutronix.de>
Reviewed-by: Manuel Traut <manut@linutronix.de>

elbepack/projectmanager.py
@@ -503,7 +503,7 @@ class ProjectManager(object):
 
         if t == 'dsc':
             ep.repo.includedsc(os.path.join(ep.builddir, filename),
-                               pkgname=pkg_name, force=True)
+                               force=True)
         elif t == 'deb':
             ep.repo.includedeb(os.path.join(ep.builddir, filename),
                                pkgname=pkg_name, force=True)