324cb508cf85a7d588a526d2f4972ee0e4380dc0
Olivier Dion <dion@linutronix.de>
2019-06-06 14:11:58 +0200

    ASCIIDocLog compatible with new logging system
    By changing the definitions of 'ASCIIDocLog::*', it's possible to use
    the new logging system already, without changing anything else.
    
    Of course, the logging objects has to be initialize first.
    
    Signed-off-by: Olivier Dion <dion@linutronix.de>
    Acked-by: Torben Hohn <torben.hohn@linutronix.de>

--- elbepack/asciidoclog.py
+++ elbepack/asciidoclog.py
@@ -7,117 +7,49 @@
 
 import os
 import sys
+import logging
 
-from elbepack.shellhelper import CommandError, command_out_stderr, command_out
-
+from elbepack.shellhelper import get_command_out, do, chroot, CommandError
 
 class LogBase(object):
-    def __init__(self, fp):
-        self.fp = fp
+    def __init__(self, *args, **kwargs):
+        pass
 
     def printo(self, text=""):
-        self.fp.write("%s\n" % str(text))
+        logging.info(text)
 
     def print_raw(self, text):
-        self.fp.write(text)
+        self.printo(text)
 
     def h1(self, text):
-        self.printo()
         self.printo(text)
-        self.printo("=" * len(str(text)))
-        self.printo()
 
     def h2(self, text):
-        self.printo()
         self.printo(text)
-        self.printo("-" * len(str(text)))
-        self.printo()
 
     def table(self):
-        self.printo("|=====================================")
+        pass
 
     def verbatim_start(self):
-        self.printo("---------------------------------------"
-                    "---------------------------------------")
+        pass
 
     def verbatim_end(self):
-        self.printo("---------------------------------------"
-                    "---------------------------------------")
-        self.printo()
+        pass
 
-    def do(self, cmd, allow_fail=False, stdin=None, env_add=None):
+    def do(self, cmd, **kwargs):
+        return do(cmd, **kwargs)
 
-        if stdin is None:
-            self.printo("running cmd +%s+" % cmd)
-        else:
-            self.printo("running cmd +%s with STDIN %s+" % (cmd, stdin))
+    def chroot(self, directory, cmd, **kwargs):
+        return chroot(directory, cmd, **kwargs)
 
-        self.verbatim_start()
-        ret, _ = command_out(cmd, stdin=stdin, output=self.fp, env_add=env_add)
-        self.verbatim_end()
-
-        if ret != 0:
-            self.printo("Command failed with errorcode %d" % ret)
-            if not allow_fail:
-                raise CommandError(cmd, ret)
-
-    def chroot(self, directory, cmd, **args):
-        os.environ["LANG"] = "C"
-        os.environ["LANGUAGE"] = "C"
-        os.environ["LC_ALL"] = "C"
-
-        chcmd = "chroot %s %s" % (directory, cmd)
-        self.do(chcmd, **args)
-
-    def get_command_out(self, cmd, allow_fail=False):
-
-        self.printo("getting output from cmd +%s+" % cmd)
-
-        ret, output, stderr = command_out_stderr(cmd)
-
-        if stderr:
-            self.verbatim_start()
-            self.print_raw(stderr)
-            self.verbatim_end()
-
-        if ret != 0:
-            self.printo("Command failed with errorcode %d" % ret)
-            if not allow_fail:
-                raise CommandError(cmd, ret)
-
-        return output
-
+    def get_command_out(self, cmd, **kwargs):
+        return get_command_out(cmd, **kwargs)
 
 class ASCIIDocLog (LogBase):
-    def __init__(self, fname, append=False):
-        self.fname = fname
-        if append:
-            fp = file(fname, "a", 0)
-        else:
-            if os.path.isfile(fname):
-                os.unlink(fname)
-            fp = file(fname, "w", 0)
-
-        LogBase.__init__(self, fp)
-
-    def reset(self):
-        self.fp.close()
-        if os.path.isfile(self.fname):
-            os.unlink(self.fname)
-        self.fp = file(self.fname, "w", 0)
-
+    pass
 
 class StdoutLog(LogBase):
-    def __init__(self):
-        LogBase.__init__(self, sys.stdout)
-
-    def reset(self):
-        pass
-
+    pass
 
 class StderrLog(LogBase):
-    def __init__(self):
-        LogBase.__init__(self, sys.stderr)
-
-    def reset(self):
-        pass
+    pass
