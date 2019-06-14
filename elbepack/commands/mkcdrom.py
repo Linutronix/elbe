dc89c5c674c8f91fc3f471c61ab61c141f2256f2
Olivier Dion <dion@linutronix.de>
2019-06-14 14:51:42 +0200

    Add logging to opt.log for command mkcdrom
    make use of the new logging system, and replace all print() with
    functions from logging and wrap it in
    "with elbe_logging({"files":opt.log}):"
    
    Signed-off-by: Olivier Dion <dion@linutronix.de>
    Acked-by: Torben Hohn <torben.hohn@linutronix.de>

--- elbepack/commands/mkcdrom.py
+++ elbepack/commands/mkcdrom.py
@@ -11,11 +11,12 @@ from __future__ import print_function
 from optparse import OptionParser
 import sys
 import os
+import logging
 
 from elbepack.elbeproject import ElbeProject
 from elbepack.elbexml import ValidationError
-from elbepack.asciidoclog import StdoutLog, ASCIIDocLog
 from elbepack.efilesystem import ChRootFilesystem
+from elbepack.log import elbe_logging
 
 from elbepack.cdroms import mk_source_cdrom, mk_binary_cdrom, CDROM_SIZE
 
@@ -61,56 +62,49 @@ def run_command(argv):
         oparser.print_help()
         sys.exit(20)
 
-    if not opt.rfs_only:
-        try:
-            project = ElbeProject(args[0], logpath=opt.log,
-                                  override_buildtype=opt.buildtype,
-                                  skip_validate=opt.skip_validation)
-        except ValidationError as e:
-            print(str(e), file=sys.stderr)
-            print("xml validation failed. Bailing out", file=sys.stderr)
-            sys.exit(20)
+    with elbe_logging({"files":opt.log}):
 
-        builddir = project.builddir
-        rfs = project.buildenv.rfs
-        xml = project.xml
-        arch = xml.text("project/arch", key="arch")
-        codename = xml.text("project/suite")
-        log = project.log
-        init_codename = xml.get_initvm_codename()
-    else:
-        builddir = os.path.abspath(os.path.curdir)
-        rfs = ChRootFilesystem(args[0])
-        arch = opt.arch
-        codename = opt.codename
-        init_codename = opt.init_codename
-        xml = None
-        if opt.log:
-            log = ASCIIDocLog(opt.log)
+        if not opt.rfs_only:
+            try:
+                project = ElbeProject(args[0], logpath=opt.log,
+                                      override_buildtype=opt.buildtype,
+                                      skip_validate=opt.skip_validation)
+            except ValidationError:
+                logging.exception("XML validation failed.  Bailing out")
+                sys.exit(20)
+
+            builddir = project.builddir
+            rfs = project.buildenv.rfs
+            xml = project.xml
+            arch = xml.text("project/arch", key="arch")
+            codename = xml.text("project/suite")
+            init_codename = xml.get_initvm_codename()
         else:
-            log = StdoutLog()
+            builddir = os.path.abspath(os.path.curdir)
+            rfs = ChRootFilesystem(args[0])
+            arch = opt.arch
+            codename = opt.codename
+            init_codename = opt.init_codename
+            xml = None
 
-    generated_files = []
-    if opt.source:
-        with rfs:
-            generated_files += mk_source_cdrom(rfs, arch, codename,
-                                               init_codename, builddir, log,
-                                               opt.cdrom_size)
+        generated_files = []
+        if opt.source:
+            with rfs:
+                generated_files += mk_source_cdrom(rfs, arch, codename,
+                                                   init_codename, builddir, log,
+                                                   opt.cdrom_size)
 
-    if opt.binary:
-        with rfs:
-            generated_files += mk_binary_cdrom(rfs,
-                                               arch,
-                                               codename,
-                                               init_codename,
-                                               xml,
-                                               builddir,
-                                               log,
-                                               opt.cdrom_size)
+        if opt.binary:
+            with rfs:
+                generated_files += mk_binary_cdrom(rfs,
+                                                   arch,
+                                                   codename,
+                                                   init_codename,
+                                                   xml,
+                                                   builddir,
+                                                   log,
+                                                   opt.cdrom_size)
 
-    print("")
-    print("Image Build finished !")
-    print("")
-    print("Files generated:")
-    for f in generated_files:
-        print(" %s" % f)
+        logging.info("Image Build finished.\n")
+        logging.info("Files generated:\n%s",
+                     "\n".join([str(f) for f in generated_files]))
