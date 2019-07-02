fe64ba6413a32c5116b44b30a3bab546e87ce94b
Torben Hohn <torben.hohn@linutronix.de>
2019-07-02 11:51:28 +0200

    changelogxml: implement class to write xml file (based on licencexml)
    abstract the writing of the chanelogxml file into its own class.
    this code is based on licencexml.
    
    add it to
    debian/python-elbe-common.install and debian/python3-elbe-common.install
    
    Signed-off-by: Torben Hohn <torben.hohn@linutronix.de>
    Reviewed-by: Bastian Germann <bage@linutronix.de>

---
+++ elbepack/changelogxml.py
@@ -0,0 +1,33 @@
+# ELBE - Debian Based Embedded Rootfilesystem Builder
+# Copyright (c) 2019 Torben Hohn <torben.hohn@linutronix.de>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+import re
+from elbepack.treeutils import etree
+
+
+remove_re = re.compile(u'[\x00-\x08\x0B-\x0C\x0E-\x1F\x7F]')
+
+
+class changelogs_xml(object):
+    def __init__(self):
+        self.outxml = etree(None)
+        self.pkglist = self.outxml.setroot('pkgchangelogs')
+
+    def add_pkg_changelog(self, xp, changelog_text):
+        changelog_text, _ = remove_re.subn('', changelog_text)
+
+        xmlpkg = self.pkglist.append('changelog')
+        xmlpkg.et.attrib['name'] = xp.name
+        xmlpkg.et.attrib['version'] = xp.candidate_version
+        xmlpkg.et.attrib['old_version'] = xp.installed_version
+
+        try:
+            xmlpkg.et.text = changelog_text.decode('utf-8')
+        except ValueError:
+            print changelog_text
+
+    def write(self, fname):
+        self.outxml.write(fname, encoding="utf-8")
