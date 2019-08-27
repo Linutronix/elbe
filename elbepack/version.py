7f842955ef94169967a30d2f2827bf3c2d3ce1bc
Torben Hohn <torben.hohn@linutronix.de>
2019-08-27 15:05:49 +0200

    debian changelog: bump version to v11
      * move <src-opts> into <src-cdrom> node
      * Update Debian codenames (buster is stable now)
      * command init: don't use user's gnupg configuration file (Fixes: #233)
      * grubinstaller: enable UEFI (secure) boot with shim
      * fix grubinstall, when /var is separate partition (Fixes: #230)
    
    Signed-off-by: Torben Hohn <torben.hohn@linutronix.de>

--- elbepack/version.py
+++ elbepack/version.py
@@ -8,7 +8,7 @@ from platform import linux_distribution
 
 from elbepack.directories import pack_dir
 
-elbe_version = "10"
+elbe_version = "11"
 running_os = linux_distribution()
 
 elbe_initvm_packagelist = ['python-elbe-buildenv',
