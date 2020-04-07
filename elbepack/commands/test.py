6db14c84e2d27a5ec5a55dec1634495720e9bc2b
Olivier Dion <dion@linutronix.de>
2020-04-07 15:03:23 -0400

    commands test: Create Elbe unit test wrapper
    Instead of invoking the tedious 'python{2,3} -m unittest discover
    [options]', this wrapper allows to simply run 'elbe test [options]'
    from anywhere in the project hierarchy.
    
    Signed-off-by: Olivier Dion <dion@linutronix.de>
    Reviewed-by: Torben Hohn <torben.hohn@linutronix.de>

---
+++ elbepack/commands/test.py
@@ -0,0 +1,16 @@
+# ELBE - Debian Based Embedded Rootfilesystem Builder
+# Copyright (c) 2020 Olivier Dion <dion@linutronix.de>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# elbepack/commands/test.py - Elbe unit test wrapper
+
+import os
+
+from elbepack.shellhelper import system
+
+def run_command(argv):
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    top_dir  = os.path.join(this_dir, "..", "..")
+    system("python -m unittest discover --start-directory '%s' %s" %
+           (top_dir, " ".join(argv)), allow_fail=True)
