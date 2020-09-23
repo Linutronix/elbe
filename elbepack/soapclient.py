46ca903f242b7720f9fef7fb7036c5624d3b7be2
Torben Hohn <torben.hohn@linutronix.de>
2020-09-23 11:19:32 +0200

    soapclient: remove b'' from wait_busy log output
    printing bytes('value') results in b'value'.
    Just print msg.
    
    Signed-off-by: Torben Hohn <torben.hohn@linutronix.de>
    Reviewed-by: Bastian Germann <bage@linutronix.de>

--- elbepack/soapclient.py
+++ elbepack/soapclient.py
@@ -636,7 +636,7 @@ class WaitProjectBusyAction(ClientAction):
             if msg == 'ELBE-FINISH':
                 break
 
-            print(msg.encode(encoding="utf-8", errors="replace"))
+            print(msg)
 
         # exited the while loop -> the project is not busy anymore,
         # check, whether everything is ok.
