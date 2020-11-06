57c34159ed640693d477ff2e6627f871225c3648
Matthias Bühler <matthias.buehler@trumpf.com>
2020-11-06 08:09:43 +0000

    Fix pkg list validation error on version wildcards
    Fix issue #276 "package list validation error when using wildcards in package
    version".  Instead of comparing version strings use regex to match versions.
    
    Signed-off-by: Matthias Bühler <matthias.buehler@trumpf.com>
    [edit commit message]
    Reviewed-by: Bastian Germann <bage@linutronix.de>

--- elbepack/dump.py
+++ elbepack/dump.py
@@ -6,6 +6,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import logging
+import re
 
 from datetime import datetime
 
@@ -106,7 +107,7 @@ def check_full_pkgs(pkgs, fullpkgs, cache):
 
             ver = p.et.get('version')
             pkg = cache.get_pkg(nomulti_name)
-            if ver and (pkg.installed_version != ver):
+            if ver and (not re.match(ver, pkg.installed_version)):
                 validation.error("Package '%s' version '%s' does not match installed version %s",
                                  name, ver, pkg.installed_version)
                 errors += 1
@@ -143,7 +144,7 @@ def check_full_pkgs(pkgs, fullpkgs, cache):
 
         pkg = cache.get_pkg(name)
 
-        if pkg.installed_version != ver:
+        if not re.match(ver, pkg.installed_version):
             validation.error("Package '%s' version %s does not match installed version %s",
                              name, ver, pkg.installed_version)
             errors += 1
