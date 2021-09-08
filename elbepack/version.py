205aeb7a15e390f5495b1a4559d166e1f8d03074
Torben Hohn <torben.hohn@linutronix.de>
2021-09-08 17:11:19 +0200

    debian: changelog: bump elbe version to 14.0
      * Team upload
      * fix pbuilder origtargz auto download
      * debianize: fix the text user interface
      * hdimg: clean up losetup() when exceptions are thrown
      * elbeproject: prevent pbuilder from checking signature when noauth is given
      * initvm: Update to bullseye
      * schema: Support <binary> for gpthd
      * many pylint fixes
    
    Signed-off-by: Torben Hohn <torben.hohn@linutronix.de>
    Reviewed-by: Bastian Germann <bage@linutronix.de>

--- elbepack/version.py
+++ elbepack/version.py
@@ -7,7 +7,7 @@
 
 from elbepack.directories import pack_dir
 
-elbe_version = "13.3"
+elbe_version = "14.0"
 
 elbe_initvm_packagelist = ['python3-elbe-buildenv',
                            'python3-elbe-soap',
