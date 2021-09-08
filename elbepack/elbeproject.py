Linutronix/elbe

commit 05b38ebcd4d6c44b475e180a216d9cc6a3d574e9

pbuilder: fix pbuilder creation for buster and stretch
Building buster pbuilder chroots fails:
--8<--
W: GPG error:
http://127.0.0.1:8080/var/cache/elbe/898da4a7-ce50-46fc-87e0-1d126bf85550/repo
buster InRelease: The following signatures couldn't be verified because the
public key is not available: NO_PUBKEY B55E6A0822C21D24
E: The repository
'http://127.0.0.1:8080/var/cache/elbe/898da4a7-ce50-46fc-87e0-1d126bf85550/repo
buster InRelease' is not signed.
--8<--
Therefore use the --keyring option of pbuilder to add the key of the
self signed repo.

For apt < 0.7.25.1 the --keyring option doesn't work, use the G hook
and 'apt-key add' to inject the key as promoted in the pbuilder man-page.

To use apt-key add directly after debootstrap, install gnupg early to avoid
--8<--
I: Copy
/var/cache/elbe/db428970-3c8e-4783-b942-3a6f792f1e44/aptconfdir/apt.conf.d  to
chroot
I: copying apt key file
/var/cache/elbe/db428970-3c8e-4783-b942-3a6f792f1e44/repo/elbe-keyring.gpg to
/var/cache/pbuilder/build/8294/etc/apt/trusted.gpg.d/elbe-keyring.gpg
I: user script /var/cache/pbuilder/build/8294/tmp/hooks/G10elbe_apt_sources
starting
E: gnupg, gnupg2 and gnupg1 do not seem to be installed, but one of them is
required for this operation
--8<--
for buster chroots.

Signed-off-by: Manuel Traut <manut@linutronix.de>
Reviewed-by: Torben Hohn <torben.hohn@linutronix.de>

elbepack/elbeproject.py
@@ -712,13 +712,14 @@ class ElbeProject (object):
                 self.builddir,
                 "pbuilder",
                 "hooks.d",
-                "D10elbe_apt_sources"))
+                "G10elbe_apt_sources"))
 
         # Run pbuilder --create
         self.log.do('pbuilder --create --configfile "%s" --aptconfdir "%s" '
-                    '--extrapackages "git gnupg"' % (
+                    '--debootstrapopts --include="git gnupg2" --keyring "%s"' % (
                         os.path.join(self.builddir, "pbuilderrc"),
-                        os.path.join(self.builddir, "aptconfdir")))
+                        os.path.join(self.builddir, "aptconfdir"),
+                        os.path.join(self.builddir, "repo", "elbe-keyring.gpg")))
 
     def sync_xml_to_disk(self):
         try:

elbepack/pbuilder.py
@@ -102,7 +102,7 @@ def pbuilder_write_repo_hook(builddir, xml):
 
     pbuilder_hook_dir = os.path.join(builddir, "pbuilder", "hooks.d")
 
-    fp = open(os.path.join(pbuilder_hook_dir, "D10elbe_apt_sources"), "w")
+    fp = open(os.path.join(pbuilder_hook_dir, "G10elbe_apt_sources"), "w")
 
     if xml.prj is None:
         return "# No Project"

commit 81621146b93db6d56712a544b07b4ee07f00c2f5

elbeproject: Fix debootstrap include syntax
The debootstrapopts in pbuilder call needs --include separated by comma.
The wrong syntax does not result in debootstrap erroring, so gnupg will be
missing if it is not included through another code path.

Fixes: 05b38ebcd4d6 ("pbuilder: fix pbuilder creation for buster and stretch")
Signed-off-by: Bastian Germann <bage@linutronix.de>
Reviewed-by: Torben Hohn <torben.hohn@linutronix.de>

elbepack/elbeproject.py
@@ -875,13 +875,13 @@ class ElbeProject:
         if cross:
             do('pbuilder --create --buildplace "%s" '
                '--configfile "%s" --aptconfdir "%s" '
-               '--debootstrapopts --include="git gnupg";' %
+               '--debootstrapopts --include="git,gnupg";' %
                (os.path.join(self.builddir, "pbuilder_cross"),
                 os.path.join(self.builddir, "cross_pbuilderrc"),
                 os.path.join(self.builddir, "aptconfdir")))
         else:
             do('pbuilder --create --configfile "%s" --aptconfdir "%s" '
-               '--debootstrapopts --include="git gnupg"' %
+               '--debootstrapopts --include="git,gnupg"' %
                (os.path.join(self.builddir, "pbuilderrc"),
                 os.path.join(self.builddir, "aptconfdir")))
 