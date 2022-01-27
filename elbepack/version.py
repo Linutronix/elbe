fd410a53728e09489259313b9584b5233b74dde8
Torben Hohn <torben.hohn@linutronix.de>
2022-01-27 11:21:05 +0100

    version: Bump elbe version in the rest of the relevant places
    Signed-off-by: Torben Hohn <torben.hohn@linutronix.de>
    Reviewed-by: Holger Dengler <dengler@linutronix.de>

--- elbepack/version.py
+++ elbepack/version.py
@@ -7,7 +7,7 @@
 
 from elbepack.directories import pack_dir
 
-elbe_version = "14.0"
+elbe_version = "14.1"
 
 elbe_initvm_packagelist = ['python3-elbe-buildenv',
                            'python3-elbe-soap',
