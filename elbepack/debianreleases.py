Linutronix/elbe

commit 04d0bbbec933bf7c4832e1cc6c79c1ac44912a05

Add schema definition for initvm/project suite restriction
If an invalid suite's name was passed in the <suite> node, then the
build would fail with a traceback, possibly after a long period.

Adding the possible suites in the XML schema allows the build to fail
at the very beginning without a traceback.

Signed-off-by: Olivier Dion <dion@linutronix.de>
Reviewed-by: Bastian Germann <bage@linutronix.de>

schema/dbsfed.xsd
@@ -313,7 +313,7 @@
           </documentation>
         </annotation>
       </element>
-      <element name="suite" type="rfs:string">
+      <element name="suite" type="rfs:suite-initvm">
         <annotation>
           <documentation>
             name of the debian suite that should be used to generate the initvm.
@@ -454,7 +454,7 @@
           </documentation>
         </annotation>
       </element>
-      <element name="suite" type="rfs:string">
+      <element name="suite" type="rfs:suite-proj">
         <annotation>
           <documentation>
             name of the debian suite that should be used to generate the
@@ -481,6 +481,24 @@
     </all>
     <attribute ref="xml:base"/>
   </complexType>
+
+  <simpleType name="suite-initvm">
+    <restriction base="string">
+      <enumeration value="bullseye" />
+      <enumeration value="buster" />
+      <enumeration value="sid" />
+      <enumeration value="stretch" />
+    </restriction>
+  </simpleType>
+
+  <simpleType name="suite-proj">
+    <restriction base="string">
+      <enumeration value="bullseye" />
+      <enumeration value="buster" />
+      <enumeration value="sid" />
+      <enumeration value="stretch" />
+    </restriction>
+  </simpleType>
 
   <complexType name="preference">
     <annotation>

commit ee0dd496cc5841714923084dceea72eb8a1c74ed

Synchronize release names in schema and code
The schema validation for release names has less valid names than the
possible codenames. Synchronize and order them historically.

Add the upcoming LTS release jammy.

Fixes: 04d0bbbec933 ("Add schema definition for initvm/project suite restriction")
Signed-off-by: Bastian Germann <bage@linutronix.de>
Reviewed-by: John Ogness <john.ogness@linutronix.de>

elbepack/debianreleases.py
@@ -26,6 +26,9 @@ suite2codename = {'oldoldoldstable': 'jessie',
                   'artful': 'artful',
                   'bionic': 'bionic',
                   'cosmic': 'cosmic',
+                  'focal': 'focal',
+                  'groovy': 'groovy',
+                  'jammy': 'jammy',
                  }
 
 

schema/dbsfed.xsd
@@ -668,25 +668,40 @@
 
   <simpleType name="suite-initvm">
     <restriction base="string">
+      <enumeration value="sid" />
       <enumeration value="bullseye" />
       <enumeration value="buster" />
-      <enumeration value="sid" />
       <enumeration value="stretch" />
     </restriction>
   </simpleType>
 
   <simpleType name="suite-proj">
     <restriction base="string">
+      <enumeration value="sid" />
       <enumeration value="bookworm" />
       <enumeration value="bullseye" />
       <enumeration value="buster" />
-      <enumeration value="sid" />
       <enumeration value="stretch" />
-      <enumeration value="bionic" />
-      <enumeration value="focal" />
+      <enumeration value="jessie" />
+
+      <enumeration value="jammy" />
       <enumeration value="groovy" />
+      <enumeration value="focal" />
+      <enumeration value="cosmic" />
+      <enumeration value="bionic" />
+      <enumeration value="artful" />
+      <enumeration value="zesty" />
+      <enumeration value="yakkety" />
       <enumeration value="xenial" />
+      <enumeration value="wily" />
+      <enumeration value="vivid" />
+      <enumeration value="utopic" />
       <enumeration value="trusty" />
+      <enumeration value="saucy" />
+      <enumeration value="raring" />
+      <enumeration value="quantal" />
+      <enumeration value="precise" />
+      <enumeration value="lucid" />
     </restriction>
   </simpleType>
 