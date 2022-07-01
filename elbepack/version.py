0f94dee59e7984bc8fb1bc0d018df021b7c9d5c3
Bastian Germann <bage@linutronix.de>
2022-07-01 10:25:14 +0200

    debian: changelog: Bump elbe version to 14.4
    Generate the changelog via gbp.
    
    Signed-off-by: Bastian Germann <bage@linutronix.de>
    Reviewed-by: John Ogness <john.ogness@linutronix.de>

--- elbepack/version.py
+++ elbepack/version.py
@@ -7,7 +7,7 @@
 
 from elbepack.directories import pack_dir
 
-elbe_version = "14.3"
+elbe_version = "14.4"
 
 elbe_initvm_packagelist = ['python3-elbe-buildenv',
                            'python3-elbe-soap',
