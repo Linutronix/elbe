Linutronix/elbe

commit 392d9ad148120283995bdba38b778059cb591e4f

Example usage of parts of ELBE in a cloud-based CI
This shows how elbe can be used in a Jenkins based CI
hosted on a kubernetes cluster.

Signed-off-by: Manuel Traut <manuel.traut@mt.com>
Reviewed-by: Bastian Germann <bage@linutronix.de>

contrib/k8s/Dockerfile
@@ -1,0 +1,29 @@
+FROM debian:bullseye-slim
+
+USER root
+
+ENV DEBIAN_FRONTEND noninteractive
+
+
+ENV LANG en_US.UTF-8
+ENV LANGUAGE en_US:en
+ENV LC_ALL en_US.UTF-8
+
+RUN apt-get update -y && \
+    apt-get install -y --no-install-recommends \
+        wget gnupg2
+
+# install current elbe
+RUN echo 'deb http://debian.linutronix.de/elbe buster main' > /etc/apt/sources.list.d/elbe.list && \
+    wget http://debian.linutronix.de/elbe/elbe-repo.pub && \
+    apt-key add elbe-repo.pub && \
+    apt-get update -y && \
+    apt-get install -y --no-install-recommends \
+        python3-elbe-buildenv \
+	locales && \
+    apt-get clean -y && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN echo "en_US.UTF-8 UTF-8" > /etc/locale.gen && locale-gen
+
+RUN echo "root:elbe" | chpasswd

contrib/k8s/example/Jenkinsfile
@@ -1,0 +1,3 @@
+@Library('elbe') _
+
+elbebuild("example-rfs.xml", true)

contrib/k8s/jenkins-shared-lib/vars/elbebuild.groovy
@@ -1,0 +1,54 @@
+def call(String elbexml, Boolean buildsdk)
+{
+  def podlabel = "elbe-${elbexml}-${UUID.randomUUID().toString()}"
+
+  properties([disableResume()])
+
+  podTemplate(label       : "${podlabel}",
+              podRetention: onFailure(),
+              containers  : [containerTemplate(name                 : 'elbe',
+                                               image                : "${env.DOCKER_REGISTRY}/elbeimage:latest",
+                                               alwaysPullImage      : true,
+                                               resourceRequestCpu   : '2000m',
+                                               resourceRequestMemory: '16Gi',
+                                               resourceLimitCpu     : '2000m',
+                                               resourceLimitMemory  : '16Gi',
+                                               ttyEnabled           : true,
+                                               privileged           : true,
+                                               command              : 'cat'),
+                            ],
+              envVars     : [containerEnvVar(key  : 'HOME',
+                                             value: '/home/jenkins/agent'),
+                            ],
+              volumes     : [persistentVolumeClaim(claimName        : 'vm-modules-pvc',
+                                                   mountPath        : '/lib/modules'),
+                            ],
+             )
+  {
+    node ("${podlabel}") {
+      stage('checkout') {
+        def gitscm = checkout scm
+        env.GIT_BRANCH = gitscm.GIT_BRANCH
+      }
+      container('elbe') {
+        stage('load kernel modules') {
+          sh "modprobe binfmt_misc"
+          sh "update-binfmts --enable qemu-arm"
+        }
+        stage('elbe image build') {
+          sh "test -d archive && elbe chg_archive ${elbexml} archive || /bin/true"
+          sh "elbe buildchroot -t image ${elbexml}"
+        }
+        stage('elbe SDK build') {
+          if (buildsdk) {
+            sh "elbe buildsdk image"
+          }
+        }
+        stage('store artifacts') {
+          sh "rm -rf image/chroot image/target image/repo image/sysroot"
+          archiveArtifacts artifacts: "image/*"
+        }
+      }
+    }
+  }
+}

contrib/k8s/vm-modules-pv.yaml
@@ -1,0 +1,14 @@
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: vm-modules-pv
+  labels:
+    type: local
+spec:
+  storageClassName: manual
+  capacity:
+    storage: 10Gi
+  accessModes:
+    - ReadWriteMany
+  hostPath:
+    path: "/lib/modules"

contrib/k8s/vm-modules-pvc.yaml
@@ -1,0 +1,12 @@
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: vm-modules-pvc
+spec:
+  storageClassName: manual
+  volumeName: vm-modules-pv
+  accessModes:
+    - ReadWriteMany
+  resources:
+    requests:
+      storage: 10Gi

commit f3ac2249374c28dc2be65af625b0663de1731f48

contrib: k8s: Add missing license info
The files were added with the signed-off commit and inherit the project's
GPL-3+ license. Add the author info as Copyright.

Fixes: 392d9ad148 ("Example usage of parts of ELBE in a cloud-based CI")
Signed-off-by: Bastian Germann <bage@linutronix.de>
Reviewed-by: Viraj Shah <viraj.shah@linutronix.de>

contrib/k8s/Dockerfile
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: Manuel Traut <manuel.traut@mt.com>
+
 FROM debian:bullseye-slim
 
 USER root

contrib/k8s/example/Jenkinsfile
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: Manuel Traut <manuel.traut@mt.com>
+
 @Library('elbe') _
 
 elbebuild("example-rfs.xml", true)

contrib/k8s/jenkins-shared-lib/vars/elbebuild.groovy
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: Manuel Traut <manuel.traut@mt.com>
+
 def call(String elbexml, Boolean buildsdk)
 {
   def podlabel = "elbe-${elbexml}-${UUID.randomUUID().toString()}"

contrib/k8s/vm-modules-pv.yaml
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: Manuel Traut <manuel.traut@mt.com>
+
 apiVersion: v1
 kind: PersistentVolume
 metadata:

contrib/k8s/vm-modules-pvc.yaml
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: Manuel Traut <manuel.traut@mt.com>
+
 apiVersion: v1
 kind: PersistentVolumeClaim
 metadata:

debian/copyright
@@ -7,6 +7,10 @@ Files: *
 Copyright: (c) 2013, Torben Hohn, Linutronix GmbH, Manuel Traut, Linutronix GmbH
 License: GPL-3+
 
+Files: contrib/k8s/*
+Copyright: Manuel Traut <manuel.traut@mt.com>
+License: GPL-3+
+
 Files: elbepack/makofiles/toolchain-shar-extract.sh.mako
 Copyright: Aníbal Limón <anibal.limon@linux.intel.com>
            Brendan Le Foll <brendan.le.foll@intel.com>