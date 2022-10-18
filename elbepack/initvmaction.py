Linutronix/elbe

commit 3204bb47629c4091cd6aca5d32dab7c75ed688de

initvmaction: Run repodir before preprocess
repodir's logical place in the command pipeline is before preprocess
because preprocess will rely on any <repodir> element to be replaced.

Add the Repodir context manager at the right place and catch RepodirError.
The web servers will be terminated with the context manager.

The rest of the change is just indenting the inner context by two levels.

Signed-off-by: Bastian Germann <bage@linutronix.de>
Reviewed-by: Kurt Kanzenbach <kurt@linutronix.de>

elbepack/initvmaction.py
@@ -15,6 +15,8 @@ import datetime
 
 import libvirt
 
+from tempfile import NamedTemporaryFile
+
 import elbepack
 from elbepack.treeutils import etree
 from elbepack.directories import elbe_exe
@@ -23,6 +25,7 @@ from elbepack.shellhelper import CommandError, system, command_out_stderr, \
 from elbepack.filesystem import TmpdirFilesystem
 from elbepack.elbexml import ElbeXML, ValidationError, ValidationMode
 from elbepack.config import cfg
+from elbepack.repodir import RepodirError, Repodir
 from elbepack.xmlpreprocess import PreprocessWrapper
 
 def is_soap_local():
@@ -255,32 +258,39 @@ class AttachAction(InitVMAction):
 
 
 
-def submit_and_dl_result(xmlfile, cdrom, opt):
+def submit_and_dl_result(input_xmlfile, cdrom, opt):
 
     # pylint: disable=too-many-statements
     # pylint: disable=too-many-branches
 
     try:
-        with PreprocessWrapper(xmlfile, opt) as ppw:
-            xmlfile = ppw.preproc
+        with NamedTemporaryFile(prefix="elbe-repodir", suffix="xml") as tmpfile:
+            xmlfile = tmpfile.name
+            with Repodir(input_xmlfile, xmlfile):
+                with PreprocessWrapper(xmlfile, opt) as ppw:
+                    xmlfile = ppw.preproc
 
-            ret, prjdir, err = command_out_stderr(
-                '%s control create_project' % (elbe_exe))
-            if ret != 0:
-                print("elbe control create_project failed.", file=sys.stderr)
-                print(err, file=sys.stderr)
-                print("Giving up", file=sys.stderr)
-                sys.exit(20)
+                    ret, prjdir, err = command_out_stderr(
+                        '%s control create_project' % (elbe_exe))
+                    if ret != 0:
+                        print("elbe control create_project failed.", file=sys.stderr)
+                        print(err, file=sys.stderr)
+                        print("Giving up", file=sys.stderr)
+                        sys.exit(20)
 
-            prjdir = prjdir.strip()
+                    prjdir = prjdir.strip()
 
-            cmd = '%s control set_xml %s %s' % (elbe_exe, prjdir, xmlfile)
-            ret, _, err = command_out_stderr(cmd)
-            if ret != 0:
-                print("elbe control set_xml failed2", file=sys.stderr)
-                print(err, file=sys.stderr)
-                print("Giving up", file=sys.stderr)
-                sys.exit(20)
+                    cmd = '%s control set_xml %s %s' % (elbe_exe, prjdir, xmlfile)
+                    ret, _, err = command_out_stderr(cmd)
+                    if ret != 0:
+                        print("elbe control set_xml failed2", file=sys.stderr)
+                        print(err, file=sys.stderr)
+                        print("Giving up", file=sys.stderr)
+                        sys.exit(20)
+    except RepodirError as err:
+        print("%s repodir failed" % elbe_exe, file=sys.stderr)
+        print(err, file=sys.stderr)
+        sys.exit(20)
     except CommandError:
         # this is the failure from PreprocessWrapper
         # it already printed the error message from

commit 834573604979dd46e13d10cbaf49bbc87981162a

initvmaction: Prevent early repodir webserver termination
The repodir resource manager needs to wrap the whole submit_and_dl_result
function to guarantee the availabilty of the served resources.

Also, create the repodir-preprocessed file in the same directory as the
original XML file so the relative archivedir semantics still work.

Fixes: 3204bb4762 ("initvmaction: Run repodir before preprocess")
Signed-off-by: Bastian Germann <bage@linutronix.de>
Tested-by: Kurt Kanzenbach <kurt@linutronix.de>
Reviewed-by: Kurt Kanzenbach <kurt@linutronix.de>

elbepack/initvmaction.py
@@ -258,39 +258,45 @@ class AttachAction(InitVMAction):
 
 
 
-def submit_and_dl_result(input_xmlfile, cdrom, opt):
+def submit_with_repodir_and_dl_result(xmlfile, cdrom, opt):
+    try:
+        with NamedTemporaryFile(suffix=".xml", prefix="elbe-repodir",
+                dir=os.path.dirname(xmlfile)) as tmpfile:
+            preprocess_xmlfile = tmpfile.name
+            with Repodir(xmlfile, preprocess_xmlfile):
+                submit_and_dl_result(preprocess_xmlfile, cdrom, opt)
+    except RepodirError as err:
+        print("%s repodir failed" % elbe_exe, file=sys.stderr)
+        print(err, file=sys.stderr)
+        sys.exit(20)
+
+
+def submit_and_dl_result(xmlfile, cdrom, opt):
 
     # pylint: disable=too-many-statements
     # pylint: disable=too-many-branches
 
     try:
-        with NamedTemporaryFile(prefix="elbe-repodir", suffix="xml") as tmpfile:
-            xmlfile = tmpfile.name
-            with Repodir(input_xmlfile, xmlfile):
-                with PreprocessWrapper(xmlfile, opt) as ppw:
-                    xmlfile = ppw.preproc
+        with PreprocessWrapper(xmlfile, opt) as ppw:
+            xmlfile = ppw.preproc
 
-                    ret, prjdir, err = command_out_stderr(
-                        '%s control create_project' % (elbe_exe))
-                    if ret != 0:
-                        print("elbe control create_project failed.", file=sys.stderr)
-                        print(err, file=sys.stderr)
-                        print("Giving up", file=sys.stderr)
-                        sys.exit(20)
+            ret, prjdir, err = command_out_stderr(
+                '%s control create_project' % (elbe_exe))
+            if ret != 0:
+                print("elbe control create_project failed.", file=sys.stderr)
+                print(err, file=sys.stderr)
+                print("Giving up", file=sys.stderr)
+                sys.exit(20)
 
-                    prjdir = prjdir.strip()
+            prjdir = prjdir.strip()
 
-                    cmd = '%s control set_xml %s %s' % (elbe_exe, prjdir, xmlfile)
-                    ret, _, err = command_out_stderr(cmd)
-                    if ret != 0:
-                        print("elbe control set_xml failed2", file=sys.stderr)
-                        print(err, file=sys.stderr)
-                        print("Giving up", file=sys.stderr)
-                        sys.exit(20)
-    except RepodirError as err:
-        print("%s repodir failed" % elbe_exe, file=sys.stderr)
-        print(err, file=sys.stderr)
-        sys.exit(20)
+            cmd = '%s control set_xml %s %s' % (elbe_exe, prjdir, xmlfile)
+            ret, _, err = command_out_stderr(cmd)
+            if ret != 0:
+                print("elbe control set_xml failed2", file=sys.stderr)
+                print(err, file=sys.stderr)
+                print("Giving up", file=sys.stderr)
+                sys.exit(20)
     except CommandError:
         # this is the failure from PreprocessWrapper
         # it already printed the error message from
@@ -646,7 +652,7 @@ class CreateAction(InitVMAction):
             elif cdrom is not None:
                 xmlfile = tmp.fname('source.xml')
 
-            submit_and_dl_result(xmlfile, cdrom, opt)
+            submit_with_repodir_and_dl_result(xmlfile, cdrom, opt)
 
 
 @InitVMAction.register('submit')
@@ -682,7 +688,7 @@ class SubmitAction(InitVMAction):
                     file=sys.stderr)
                 sys.exit(20)
 
-            submit_and_dl_result(xmlfile, cdrom, opt)
+            submit_with_repodir_and_dl_result(xmlfile, cdrom, opt)
 
 @InitVMAction.register('sync')
 class SyncAction(InitVMAction):