Linutronix/elbe

commit 358d4850064c6cc61b02cfb18a2019d8b7edcd28

Replace gnupg dependencies with gpg
The gnupg package has evolved to include more dependencies than gpg that
are not necessary on every target.
Depend on gpg instead to get rid of the additional dependencies.

Signed-off-by: Bastian Germann <bage@linutronix.de>
Reviewed-by: John Ogness <john.ogness@linutronix.de>

elbepack/elbeproject.py
@@ -873,12 +873,12 @@ class ElbeProject:
                f'--buildplace "{os.path.join(self.builddir, "pbuilder_cross")}" '
                f'--configfile "{os.path.join(self.builddir, "cross_pbuilderrc")}" '
                f'--aptconfdir "{os.path.join(self.builddir, "aptconfdir")}" '
-               f'--debootstrapopts --include="git,gnupg" {no_check_gpg};')
+               f'--debootstrapopts --include="git,gpg" {no_check_gpg};')
         else:
             do('pbuilder --create '
                f'--configfile "{os.path.join(self.builddir, "pbuilderrc")}" '
                f'--aptconfdir "{os.path.join(self.builddir, "aptconfdir")}" '
-               f'--debootstrapopts --include="git,gnupg" {no_check_gpg}')
+               f'--debootstrapopts --include="git,gpg" {no_check_gpg}')
 
     def sync_xml_to_disk(self):
         try:

elbepack/rfs.py
@@ -187,7 +187,7 @@ class BuildEnv:
             strapcmd += f" --variant={self.xml.text('target/debootstrap/variant')}"
 
         # Should we include additional packages into bootstrap?
-        includepkgs = "gnupg"  # These are the packages which are included in any case
+        includepkgs = "gpg"  # These are the packages which are included in any case
         if self.xml.has("target/debootstrap/include"):
             includepkgs += f", {self.xml.text('target/debootstrap/include')}"
         strapcmd += f" --include=\"{includepkgs}\""

commit cc2956aafc76b3c45bf7caef31f5e9c09d2fe27e

elbeproject: Install full gnupg for pbuilder again
As pbuilder still relies on apt-key, it needs gpg-agent to be available.
Go back to installing it via debootstrap.

Fixes: 358d485006 ("Replace gnupg dependencies with gpg")
Signed-off-by: Bastian Germann <bage@linutronix.de>
Reviewed-by: John Ogness <john.ogness@linutronix.de>

elbepack/elbeproject.py
@@ -872,12 +872,12 @@ class ElbeProject:
                f'--buildplace "{os.path.join(self.builddir, "pbuilder_cross")}" '
                f'--configfile "{os.path.join(self.builddir, "cross_pbuilderrc")}" '
                f'--aptconfdir "{os.path.join(self.builddir, "aptconfdir")}" '
-               f'--debootstrapopts --include="git,gpg" {no_check_gpg};')
+               f'--debootstrapopts --include="git,gnupg" {no_check_gpg};')
         else:
             do('pbuilder --create '
                f'--configfile "{os.path.join(self.builddir, "pbuilderrc")}" '
                f'--aptconfdir "{os.path.join(self.builddir, "aptconfdir")}" '
-               f'--debootstrapopts --include="git,gpg" {no_check_gpg}')
+               f'--debootstrapopts --include="git,gnupg" {no_check_gpg}')
 
     def sync_xml_to_disk(self):
         try: