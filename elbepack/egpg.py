Linutronix/elbe

commit 3251954d6b27d205cc57f10defa5464e37bcd02e

egpg: Implement unarmor_openpgp_keyring
Add a function to convert an ascii-armored OpenPGP key to its binary
representation.

Signed-off-by: Bastian Germann <bage@linutronix.de>
Reviewed-by: John Ogness <john.ogness@linutronix.de>

elbepack/egpg.py
@@ -5,6 +5,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import logging
 import os
 
 from gpg import core
@@ -12,7 +13,7 @@ from gpg.constants import sigsum, sig, PROTOCOL_OpenPGP
 from gpg.errors import GPGMEError, KeyNotFound, InvalidSigners
 
 from elbepack.filesystem import hostfs
-from elbepack.shellhelper import system
+from elbepack.shellhelper import get_command_out, system
 
 elbe_internal_key_param = """
 <GnupgKeyParms format="internal">
@@ -272,3 +273,16 @@ def generate_elbe_internal_key():
 def export_key(fingerprint, outfile):
     system(f"/usr/bin/gpg -a -o {outfile} --export {fingerprint}",
            env_add={"GNUPGHOME": "/var/cache/elbe/gnupg"})
+
+def unarmor_openpgp_keyring(armored):
+    """
+    Unarmors one ascii-armored (string) OpenPGP keyring.
+    Returns a binary string (empty for invalid keys).
+    """
+    try:
+        conv_cmd = get_command_out('/usr/bin/gpg --no-options --dearmor', stdin=armored)
+    except CommandError as e:
+        logging.error(e)
+        return b""
+
+    return conv_cmd

commit 41db956427a75dc87d95d7cc1b0b03f8b1d72f5f

egpg: Add missing import
CommandError was introduced without importing it.
Add the missing import.

Fixes: 3251954d6b ("egpg: Implement unarmor_openpgp_keyring")
Signed-off-by: Bastian Germann <bage@linutronix.de>
Reviewed-by: Ros Spolyak <rostokus@gmail.com>

elbepack/egpg.py
@@ -13,7 +13,7 @@ from gpg.constants import sigsum, sig, PROTOCOL_OpenPGP
 from gpg.errors import GPGMEError, KeyNotFound, InvalidSigners
 
 from elbepack.filesystem import hostfs
-from elbepack.shellhelper import get_command_out, system
+from elbepack.shellhelper import CommandError, get_command_out, system
 
 elbe_internal_key_param = """
 <GnupgKeyParms format="internal">