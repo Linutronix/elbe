266654037f0d4e91ba972fe182a88dd2b5823a4d
Benedikt Spranger <b.spranger@linutronix.de>
2023-09-11 16:49:38 +0200

    Indentation fix
    Unfortunately commit fdc2e8623adc ("elbepack: commands:
    fetch_initvm_pkgs.py: retries for package-fetching added") introduced an
    indentation error:
    
      Sorry: IndentationError: unindent does not match any outer indentation
      level (fetch_initvm_pkgs.py, line 179)
    
    While at it, fix also all other indentation errors where 6 instead of 4
    spaces where used for indentation.
    
    Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>
    Signed-off-by: Volker Haspel <volker.haspel@linutronix.de>
    Reviewed-by: Eduard Krein <eduard@linutronix.de>
    Tested-by: John Ogness <john.ogness@linutronix.de>

--- elbepack/commands/fetch_initvm_pkgs.py
+++ elbepack/commands/fetch_initvm_pkgs.py
@@ -112,30 +112,32 @@ def run_command(argv):
                 pkg_id = f"{pkg.name}-{pkg.installed_version}"
                 retry = 1
                 while retry < 3:
-                      try:
-                          p = cache[pkg.name]
-                          pkgver = p.installed
-                          deb = fetch_binary(pkgver,
-                                       opt.archive,
-                                       ElbeAcquireProgress(cb=None))
-                          repo.includedeb(deb, 'main', prio=pkgver.priority)
-                          break
-                      except ValueError:
-                          logging.exception('No package "%s"', pkg_id)
-                          retry = 3
-                      except FetchError:
-                          logging.exception('Package "%s-%s" could not be downloaded',
-                                      pkg.name, pkgver.version)
-                          retry += 1
-                      except TypeError:
-                          logging.exception('Package "%s" missing name or version', pkg_id)
-                          retry = 3
-                      except CommandError:
-                          logging.exception('Package "%s-%s" could not be added to repo.',
+                    try:
+                        p = cache[pkg.name]
+                        pkgver = p.installed
+                        deb = fetch_binary(pkgver,
+                                           opt.archive,
+                                           ElbeAcquireProgress(cb=None))
+                        repo.includedeb(deb, 'main', prio=pkgver.priority)
+                        break
+                    except ValueError:
+                        logging.exception('No package "%s"', pkg_id)
+                        retry = 3
+                    except FetchError:
+                        logging.exception('Package "%s-%s" could not be downloaded',
                                           pkg.name, pkgver.version)
-                          retry += 1
-                if retry >= 3:
-                    logging.error('Failed to get binary Package "%s"', pkg_id)
+                        retry += 1
+                    except TypeError:
+                        logging.exception('Package "%s" missing name or version',
+                                          pkg_id)
+                        retry = 3
+                    except CommandError:
+                        logging.exception('Package "%s-%s" could not be added to repo.',
+                                          pkg.name, pkgver.version)
+                        retry += 1
+                    if retry >= 3:
+                        logging.error('Failed to get binary Package "%s"',
+                                      pkg_id)
 
         repo.finalize()
 
@@ -156,29 +158,32 @@ def run_command(argv):
                 pkg_id = f"{pkg.name}-{pkg.installed_version}"
                 retry = 1
                 while retry < 3:
-                      try:
-                          p = cache[pkg.name]
-                          pkgver = p.installed
-                          dsc = pkgver.fetch_source(opt.srcarchive,
-                                              ElbeAcquireProgress(cb=None),
-                                              unpack=False)
-                          repo.include_init_dsc(dsc, 'initvm')
-                          break
-                      except ValueError:
-                          logging.exception('No package "%s"', pkg_id)
-                          retry = 3
-                      except FetchError:
-                          logging.exception('Package "%s-%s" could not be downloaded', pkg.name, pkgver.version)
-                          retry += 1
-                      except TypeError:
-                          logging.exception('Package "%s" missing name or version', pkg_id)
-                          retry = 3
-                      except CommandError:
-                          logging.exception('Package "%s-%s" could not be added to repo.',
+                    try:
+                        p = cache[pkg.name]
+                        pkgver = p.installed
+                        dsc = pkgver.fetch_source(opt.srcarchive,
+                                                  ElbeAcquireProgress(cb=None),
+                                                  unpack=False)
+                        repo.include_init_dsc(dsc, 'initvm')
+                        break
+                    except ValueError:
+                        logging.exception('No package "%s"', pkg_id)
+                        retry = 3
+                    except FetchError:
+                        logging.exception('Package "%s-%s" could not be downloaded',
                                           pkg.name, pkgver.version)
-                    retry += 1
-                if retry >= 3:
-                    logging.error('Failed to get source Package "%s"', pkg_id)
+                        retry += 1
+                    except TypeError:
+                        logging.exception('Package "%s" missing name or version',
+                                          pkg_id)
+                        retry = 3
+                    except CommandError:
+                        logging.exception('Package "%s-%s" could not be added to repo.',
+                                          pkg.name, pkgver.version)
+                        retry += 1
+                    if retry >= 3:
+                        logging.error('Failed to get source Package "%s"',
+                                      pkg_id)
 
         repo.finalize()
 
