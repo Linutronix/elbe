5d1454cfd08441b3648cbc6871b64f6eb8ad2e49
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-02-13 09:53:30 +0100

    elbepack: test_preproc: migrate to subtests
    The custom test-parameterization is not compatible with pytest.
    As pytest is more featureful than our own test runner prepare for
    switching over.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Reviewed-by: Benedikt Spranger <b.spranger@linutronix.de>

--- elbepack/tests/test_preproc.py
+++ elbepack/tests/test_preproc.py
@@ -10,14 +10,15 @@ from elbepack.directories import elbe_dir, elbe_exe
 
 
 class TestPreproc(ElbeTestCase):
-    params = [os.path.join(elbe_dir, 'tests', fname)
-              for fname
-              in os.listdir(os.path.join(elbe_dir, 'tests'))
-              if fname.startswith('preproc') and fname.endswith('.xml')]
-
     def test_preproc(self):
-        try:
-            system(f'{sys.executable} {elbe_exe} preprocess "{self.param}"')
-        except ElbeTestException as e:
-            self.stdout = e.out
-            raise
+        for param in [os.path.join(elbe_dir, 'tests', fname)
+                      for fname
+                      in os.listdir(os.path.join(elbe_dir, 'tests'))
+                      if fname.startswith('preproc') and fname.endswith('.xml')]:
+
+            with self.subTest(file=param):
+                try:
+                    system(f'{sys.executable} {elbe_exe} preprocess "{param}"')
+                except ElbeTestException as e:
+                    self.stdout = e.out
+                    raise
