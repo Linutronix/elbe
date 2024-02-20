0c846bba39aacf22aa1852b29a4414f7f7ce60f8
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-02-20 17:48:35 +0100

    elbepack: test_doctest; switch to pytest APIs
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Reviewed-by: Benedikt Spranger <b.spranger@linutronix.de>

--- elbepack/tests/test_doctest.py
+++ elbepack/tests/test_doctest.py
@@ -6,15 +6,14 @@ import doctest
 
 import elbepack.filesystem as filesystem
 import elbepack.shellhelper as shellhelper
-from elbepack.commands.test import ElbeTestCase
 
 
-class ElbeDocTest(ElbeTestCase):
-    def test_shellhelper(self):
-        fail, _ = doctest.testmod(shellhelper)
-        self.assertEqual(fail, 0)
+def test_shellhelper():
+    fail, _ = doctest.testmod(shellhelper)
+    assert fail == 0
 
-    def test_filesystem(self):
-        with filesystem.TmpdirFilesystem() as this:
-            fail, _ = doctest.testmod(filesystem, extraglobs={'this': this})
-        self.assertEqual(fail, 0)
+
+def test_filesystem():
+    with filesystem.TmpdirFilesystem() as this:
+        fail, _ = doctest.testmod(filesystem, extraglobs={'this': this})
+    assert fail == 0
