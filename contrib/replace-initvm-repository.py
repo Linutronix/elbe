50dc60d7b7f41cdf54e940389a53d696163bfb5a
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-02-20 12:49:44 +0100

    contrib: add script to modify initvm repositories during ci
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Reviewed-by: Benedikt Spranger <b.spranger@linutronix.de>

---
+++ contrib/replace-initvm-repository.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import argparse
+import xml.etree.ElementTree as ET
+
+
+def replace_repo(doc, old_repo_url, new_repo_url, key):
+    for url in doc.findall('./initvm/mirror/url-list/url'):
+        binary = url.find('binary')
+        if binary.text.strip() != old_repo_url:
+            continue
+        binary.text = new_repo_url
+        url.find('source').text = new_repo_url
+        url.find('raw-key').text = '\n' + key + '\n'
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('xml')
+    parser.add_argument('old-repo-url')
+    parser.add_argument('new-repo-url')
+    parser.add_argument('keyfile')
+
+    args = parser.parse_args()
+
+    with open(args.keyfile, 'r') as keyfile:
+        key = keyfile.read()
+
+    doc = ET.parse(args.xml)
+
+    replace_repo(
+            doc,
+            vars(args)['old-repo-url'],
+            vars(args)['new-repo-url'],
+            key,
+    )
+
+    ET.dump(doc)
