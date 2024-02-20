30232db3595322679714ea1b0203990e8edb24be
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-02-20 17:36:02 +0100

    elbepack: test_flake8; switch to pytest APIs
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Reviewed-by: Benedikt Spranger <b.spranger@linutronix.de>

--- elbepack/tests/test_flake8.py
+++ elbepack/tests/test_flake8.py
@@ -2,26 +2,27 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileCopyrightText: 2020 Linutronix GmbH
 
-from elbepack.commands.test import ElbeTestCase, ElbeTestException, system
+import glob
+
+from elbepack.commands.test import ElbeTestException, system
 from elbepack.directories import elbe_exe, pack_dir
-from elbepack.shellhelper import system_out
+
+import pytest
 
 
-class TestPylint(ElbeTestCase):
-    flake8_opts = ['--max-line-length=100',
-                   '--show-source']
+flake8_opts = ['--max-line-length=100',
+               '--show-source']
 
-    def test_lint(self):
-        files = system_out(f"find {pack_dir} -iname '*.py'").splitlines()
-        files.append(elbe_exe)
 
-        for f in files:
-            with self.subTest(file=f):
-                err_out = None
-                try:
-                    system(f"flake8 {' '.join(self.flake8_opts)} {f}")
-                except ElbeTestException as e:
-                    err_out = e.out
+def _python_files():
+    files = glob.glob(pack_dir + '/**/*.py', recursive=True)
+    files.append(elbe_exe)
+    return files
 
-                if err_out is not None:
-                    self.fail(err_out)
+
+@pytest.mark.parametrize('f', _python_files())
+def test_flake8(f):
+    try:
+        system(f'flake8 {" ".join(flake8_opts)} {f}')
+    except ElbeTestException as e:
+        pytest.fail(e.out)
