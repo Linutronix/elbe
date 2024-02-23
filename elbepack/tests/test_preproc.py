63e828a73ff6edc8b54bc99d14417ca5b6d37b79
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-02-23 16:17:54 +0100

    elbepack: allow xinclude on check-image-list
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- elbepack/tests/test_preproc.py
+++ elbepack/tests/test_preproc.py
@@ -10,29 +10,14 @@ from elbepack.directories import elbe_dir, elbe_exe
 
 
 class TestPreproc(ElbeTestCase):
-    failure_set = {os.path.join(elbe_dir, path)
-                   for path
-                   in [
-                       'tests/preproc-01.xml'
-                   ]}
-
     params = [os.path.join(elbe_dir, 'tests', fname)
               for fname
               in os.listdir(os.path.join(elbe_dir, 'tests'))
               if fname.startswith('preproc') and fname.endswith('.xml')]
 
     def test_preproc(self):
-
         try:
             system(f'{sys.executable} {elbe_exe} preprocess "{self.param}"')
         except ElbeTestException as e:
-            if self.param in TestPreproc.failure_set:
-                self.stdout = e.out
-                self.skipTest(
-                    f'Preproc test for {self.param} is expected to fail')
-            else:
-                raise
-        else:
-            if self.param in TestPreproc.failure_set:
-                raise Exception(f'Preproc test for {self.param}'
-                      'is expected to fail, but did not !')
+            self.stdout = e.out
+            raise
