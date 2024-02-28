ebec20efbb7fc82ae8f5cdfc6868440d430b3d36
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-02-28 13:36:02 +0100

    elbepack: drop function init_directories
    Instead of having to call init_directories() manually during startup use
    the __main__ module to do the same automatically.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Reviewed-by: Benedikt Spranger <b.spranger@linutronix.de>

--- elbepack/directories.py
+++ elbepack/directories.py
@@ -2,23 +2,16 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileCopyrightText: 2015, 2017 Linutronix GmbH
 
+import __main__
 import os
 from pkgutil import iter_modules
 
 import elbepack
 
-elbe_exe = None
-elbe_dir = None
 
-
-def init_directories(elbe_relpath):
-    # set global variables that are used in other modules via imports
-    # this is the very first function that is called by 'elbe'
-    global elbe_exe
-    global elbe_dir
-
-    elbe_exe = os.path.abspath(os.path.realpath(elbe_relpath))
-    elbe_dir = os.path.dirname(elbe_exe)
+# set global variables that are used in other modules via imports
+elbe_exe = os.path.abspath(os.path.realpath(__main__.__file__))
+elbe_dir = os.path.dirname(elbe_exe)
 
     if not elbe_exe.startswith('/usr/bin/'):
 
