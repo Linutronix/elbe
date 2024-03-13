ea5c3902f1e0350efbb83c47d46275e045e70a03
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-03-13 16:29:29 +0100

    test_flake8: only call flake8 once
    It is much faster than calling flake8 for each file and also allows the
    removal of pack_dir.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Reviewed-by: Eduard Krein <eduard.krein@linutronix.de>

--- elbepack/tests/test_flake8.py
+++ elbepack/tests/test_flake8.py
@@ -2,20 +2,12 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileCopyrightText: 2020 Linutronix GmbH
 
-import glob
 import subprocess
 
-from elbepack.directories import elbe_exe, pack_dir
 
-import pytest
-
-
-def _python_files():
-    files = glob.glob(pack_dir + '/**/*.py', recursive=True)
-    files.append(elbe_exe)
-    return files
-
-
-@pytest.mark.parametrize('f', _python_files())
-def test_flake8(f):
-    subprocess.run(['flake8', '--max-line-length=100', '--show-source', f], check=True)
+def test_flake8():
+    subprocess.run([
+        'flake8',
+        '--max-line-length=100', '--show-source',
+        'elbepack',
+    ], check=True)
