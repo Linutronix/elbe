6ff3fb041a13cfae056c0414b70573f2b417fd28
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-03-14 10:01:19 +0100

    elbepack: tests: introduce helpers for xml testcases
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Reviewed-by: Eduard Krein <eduard.krein@linutronix.de>

---
+++ elbepack/tests/__init__.py
@@ -0,0 +1,26 @@
+import pathlib
+
+import pytest
+
+
+def xml_test_files(prefix):
+    return [
+        file
+        for file
+        in pathlib.Path('tests').iterdir()
+        if file.name.startswith(prefix) and file.name.endswith('.xml')
+    ]
+
+
+def parametrize_xml_test_files(name, prefix):
+    """ Decorator to parametrize test functions for matching test XML files """
+
+    files = xml_test_files(prefix)
+
+    def wrapper(func):
+        return pytest.mark.parametrize(
+                name, files,
+                ids=[file.name for file in files]
+        )(func)
+
+    return wrapper
