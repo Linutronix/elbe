ead82e529a854e7b8af3cd0a067b2b943b3b240f
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-04-03 11:34:32 +0200

    tests: validate the output of preprocessing
    Instead of testing only the preprocessing does not fail also validate
    its output.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Reviewed-by: Eduard Krein <eduard.krein@linutronix.de>

--- elbepack/tests/test_preproc.py
+++ elbepack/tests/test_preproc.py
@@ -2,10 +2,28 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileCopyrightText: 2020-2021 Linutronix GmbH
 
+import pathlib
+
+import pytest
+
 from elbepack.directories import run_elbe
-from elbepack.tests import parametrize_xml_test_files
 
 
-@parametrize_xml_test_files('f', 'preproc')
-def test_preproc(f):
-    run_elbe(['preprocess', f], check=True)
+def _test_file_path(name):
+    return pathlib.Path(__file__).parents[2].joinpath('tests', name)
+
+
+@pytest.mark.parametrize(('inp', 'out'), [
+    ('preproc-01.xml', 'preproc-01.out.xml'),
+    ('preproc-02.xml', 'preproc-02.out.xml'),
+])
+def test_preproc(inp, out, tmp_path):
+    actual = tmp_path / (inp + '.out')
+    run_elbe([
+        'preprocess',
+        '-z', '0',
+        '-o', actual,
+        _test_file_path(inp),
+    ], check=True)
+    with open(_test_file_path(out)) as expected, actual.open() as a:
+        assert a.read() == expected.read()
