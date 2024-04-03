f061aa67271ad6274108f73c014132e176246d2c
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-04-03 14:57:09 +0200

    tests: validate variant preprocessing
    Test that variant processing works correctly, producing the correct
    output data.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Reviewed-by: Eduard Krein <eduard.krein@linutronix.de>

--- elbepack/tests/test_preproc.py
+++ elbepack/tests/test_preproc.py
@@ -13,15 +13,20 @@ def _test_file_path(name):
     return pathlib.Path(__file__).parents[2].joinpath('tests', name)
 
 
-@pytest.mark.parametrize(('inp', 'out'), [
-    ('preproc-01.xml', 'preproc-01.out.xml'),
-    ('preproc-02.xml', 'preproc-02.out.xml'),
+@pytest.mark.parametrize(('inp', 'out', 'variant'), [
+    ('preproc-01.xml', 'preproc-01.out.xml', None),
+    ('preproc-02.xml', 'preproc-02.out.xml', None),
+    ('preproc-variants.xml', 'preproc-variants-none.out.xml', None),
+    ('preproc-variants.xml', 'preproc-variants-variant1.out.xml', 'variant1'),
+    ('preproc-variants.xml', 'preproc-variants-variant2.out.xml', 'variant2'),
+    ('preproc-variants.xml', 'preproc-variants-variant1,variant2.out.xml', 'variant1,variant2'),
 ])
-def test_preproc(inp, out, tmp_path):
+def test_preproc(inp, out, variant, tmp_path):
     actual = tmp_path / (inp + '.out')
     run_elbe([
         'preprocess',
         '-z', '0',
+        *(['-v', variant] if variant else []),
         '-o', actual,
         _test_file_path(inp),
     ], check=True)
