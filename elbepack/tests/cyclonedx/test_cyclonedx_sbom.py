4426e6d011781dc4a1e358cc66ebcf253596a436
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-07-05 15:11:01 +0200

    elbepack: cyclonedx-sbom: use valid serialNumber in reference tests
    We want the reference data itself to satisfy the schema.
    Instead of the non-UUID string "INVALID" use the null UUID.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Reviewed-by: Eduard Krein<eduard.krein@linutronix.de>

--- elbepack/tests/cyclonedx/test_cyclonedx_sbom.py
+++ elbepack/tests/cyclonedx/test_cyclonedx_sbom.py
@@ -4,6 +4,7 @@
 
 import json
 import pathlib
+import uuid
 
 import jsonschema
 
@@ -30,7 +31,7 @@ def test_schema():
 def test_reference_data():
     test_bom = generate_test_bom()
     test_bom['metadata']['timestamp'] = 'INVALID'
-    test_bom['serialNumber'] = 'INVALID'
+    test_bom['serialNumber'] = uuid.UUID(int=0).urn
     test_bom['metadata']['tools'][0]['version'] = 'INVALID'
     with here.joinpath('cyclonedx_reference.json').open() as f:
         reference_data = json.load(f)
