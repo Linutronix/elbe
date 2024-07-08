f5bfffcaacc5ba58e8027012ac9312f735b7ed10
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-07-08 11:54:42 +0200

    elbepack: validate: migrate to argparse
    argparse has various advantages over optparse:
    
    * Autogenerated command synopsis.
    * Required arguments.
    * Flexible argument types.
    * Subparsers.
    
    Furthermore optparse is deprecated since Python 3.2 (2011).
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- elbepack/commands/validate.py
+++ elbepack/commands/validate.py
@@ -2,41 +2,38 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileCopyrightText: 2013-2017 Linutronix GmbH
 
+import argparse
 import os
 import sys
-from optparse import OptionParser
 
 from elbepack.elbexml import ElbeXML, ValidationError, ValidationMode
 from elbepack.validate import validate_xml
 
 
 def run_command(argv):
-    oparser = OptionParser(usage='usage: %prog validate <xmlfile>')
-    oparser.add_option('--validate-urls', dest='validate_urls',
-                       help='try to access specified repositories',
-                       default=False, action='store_true')
+    aparser = argparse.ArgumentParser(prog='elbe validate')
+    aparser.add_argument('--validate-urls', dest='validate_urls',
+                         help='try to access specified repositories',
+                         default=False, action='store_true')
+    aparser.add_argument('xmlfile')
 
-    (opt, args) = oparser.parse_args(argv)
+    args = aparser.parse_args(argv)
 
-    if not args:
-        oparser.print_help()
-        sys.exit(58)
-
-    if not os.path.exists(args[0]):
-        print(f'{args[0]} - file not found')
-        oparser.print_help()
+    if not os.path.exists(args.xmlfile):
+        print(f'{args.xmlfile} - file not found')
+        aparser.print_help()
         sys.exit(59)
 
-    validation = validate_xml(args[0])
+    validation = validate_xml(args.xmlfile)
     if validation:
         print('validation failed')
         for i in validation:
             print(i)
         sys.exit(60)
 
-    if opt.validate_urls:
+    if args.validate_urls:
         try:
-            ElbeXML(args[0], url_validation=ValidationMode.CHECK_ALL)
+            ElbeXML(args.xmlfile, url_validation=ValidationMode.CHECK_ALL)
         except ValidationError as e:
             print(e)
             sys.exit(61)
