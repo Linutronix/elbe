a7283ff948999c97c22044ee1b7cfae25e6711c3
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-07-19 10:24:28 +0200

    elbepack: projectmanager: drop unused function get_current_project_data
    This function is never used, so delete it.
    Also propagate the deletion to other components which are now unused.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>
    Reviewed-by: Benedikt Spranger <b.spranger@linutronix.de>

--- elbepack/projectmanager.py
+++ elbepack/projectmanager.py
@@ -165,11 +165,6 @@ class ProjectManager:
 
         self.db.del_project(builddir)
 
-    def get_current_project_data(self, userid):
-        with self.lock:
-            builddir = self._get_current_project(userid).builddir
-            return self.db.get_project_data(builddir)
-
     def get_current_project_files(self, userid):
         with self.lock:
             builddir = self._get_current_project(userid).builddir
