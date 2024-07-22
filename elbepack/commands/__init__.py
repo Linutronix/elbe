962c82a953346d7b50e0d8a2c7fba669baf3ff02
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-07-22 08:45:16 +0200

    elbepack: commands: add deprecated argument helper for argparse
    All usage of optparse is being replaced with argparse.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- elbepack/commands/__init__.py
+++ elbepack/commands/__init__.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileCopyrightText: 2024 Linutronix GmbH
 
+import argparse
 import optparse
 import sys
 
@@ -14,3 +15,14 @@ def add_deprecated_optparse_option(oparser, *args, **kwargs):
     oparser.add_option(*args, **kwargs,
                        action='callback', callback=_deprecated_option_cb,
                        help=optparse.SUPPRESS_HELP)
+
+
+class _DeprecatedArgumentAction(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string):
+        print(f'Deprecated option "{option_string}" was used. This option is a NOOP.',
+              file=sys.stderr)
+
+
+def add_deprecated_argparse_argument(parser, *args, **kwargs):
+    parser.add_argument(*args, **kwargs, action=_DeprecatedArgumentAction,
+                        help=argparse.SUPPRESS)
