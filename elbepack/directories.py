3c289ee9a1a4cfd551d4c587c373864cb8e5948c
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-07-25 08:09:33 +0200

    elbepack: directories: introduce helper to execute an elbe subcommand
    In contrast to the existing run_elbe() helper,
    run_elbe_subcommand() executes the subcommand in the same
    interpreter/process.
    This has the advantage that all environment and stacktraces are
    preserved.
    For now this is useful for unittests but as soon as the exception-based
    error handling is active it can be used throughout elbe.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- elbepack/directories.py
+++ elbepack/directories.py
@@ -2,9 +2,18 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileCopyrightText: 2015, 2017 Linutronix GmbH
 
+import os
 import subprocess
 import sys
+
+import elbepack.main
 
 
 def run_elbe(args, **kwargs):
     return subprocess.run([sys.executable, '-melbepack', *args], **kwargs)
+
+
+def run_elbe_subcommand(args):
+    return elbepack.main.main([
+        'elbe', *[os.fspath(arg) for arg in args],
+    ])
