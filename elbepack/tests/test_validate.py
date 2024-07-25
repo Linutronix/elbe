ef2526daf307b7016b959c7f2515737a1344597b
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-07-25 08:12:23 +0200

    tests: test_validate: migrate to run_elbe_subcommand
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- elbepack/tests/test_validate.py
+++ elbepack/tests/test_validate.py
@@ -7,7 +7,7 @@ import os
 
 import pytest
 
-from elbepack.directories import run_elbe
+from elbepack.directories import run_elbe_subcommand
 
 
 def _test_cases():
@@ -31,5 +31,5 @@ def _examples():
 @pytest.mark.parametrize('f', itertools.chain(_test_cases(), _examples()))
 def test_validate(f, tmp_path):
     p = tmp_path / 'preprocessed.xml'
-    run_elbe(['preprocess', '-o', p, f], check=True)
-    run_elbe(['validate', p], check=True)
+    run_elbe_subcommand(['preprocess', '-o', p, f])
+    run_elbe_subcommand(['validate', p])
