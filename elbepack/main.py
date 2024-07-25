869c2f2e8d35678f8d42a3ddd6be33a7a26115ab
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-07-25 07:54:35 +0200

    elbepack: main: allow specification of custom argv
    To execute elbe subcommands from other parts of elbe it is useful to
    explicitly specify an argv to use.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- elbepack/main.py
+++ elbepack/main.py
@@ -22,28 +22,28 @@ def usage():
         print('        * %s' % i)
 
 
-def main():
+def main(argv=sys.argv):
     # with python -melbepack the optparse usage message would show __main__.py
-    if sys.argv[0].endswith('__main__.py'):
-        sys.argv[0] = 'elbe'
+    if argv[0].endswith('__main__.py'):
+        argv[0] = 'elbe'
 
-    if len(sys.argv) < 2:
+    if len(argv) < 2:
         usage()
         sys.exit(20)
 
-    if sys.argv[1] == '--version':
+    if argv[1] == '--version':
         print('elbe v%s' % (elbe_version))
         sys.exit(0)
 
     cmd_list = get_cmdlist()
 
-    if sys.argv[1] not in cmd_list:
+    if argv[1] not in cmd_list:
         print('Unknown subcommand !\n')
         usage()
         sys.exit(20)
 
-    modname = 'elbepack.commands.' + sys.argv[1]
+    modname = 'elbepack.commands.' + argv[1]
 
     cmdmod = importlib.import_module(modname)
 
-    cmdmod.run_command(sys.argv[2:])
+    cmdmod.run_command(argv[2:])
