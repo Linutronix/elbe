a43939a88c5819f83ee98dc74f7d14b89f7f9f27
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-07-30 09:48:55 +0200

    elbepack: remove_sign: use argparse for CLI parsing
    Provide more uniform error messages.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- elbepack/commands/remove_sign.py
+++ elbepack/commands/remove_sign.py
@@ -2,16 +2,18 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileCopyrightText: 2014, 2017 Linutronix GmbH
 
+import argparse
+
 from elbepack.egpg import unsign_file
 
 
 def run_command(argv):
-    if len(argv) != 1:
-        print('Wrong number of arguments.')
-        print('Please pass the name of the file to unsign.')
-        return
+    parser = argparse.ArgumentParser(prog='elbe remove_sign')
+    parser.add_argument('file', help='file to unsign')
 
-    fname = unsign_file(argv[0])
+    args = parser.parse_args(argv)
+
+    fname = unsign_file(args.file)
     if fname:
         print(f'unsigned file: {fname}')
     else:
