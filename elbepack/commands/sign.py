65fc5d5cbf913ffa560e5b57a50f09e8eb269b1e
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-07-30 09:46:30 +0200

    elbepack: sign: use argparse for CLI parsing
    Provide more uniform error messages.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- elbepack/commands/sign.py
+++ elbepack/commands/sign.py
@@ -2,13 +2,16 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 # SPDX-FileCopyrightText: 2014-2015, 2017 Linutronix GmbH
 
+import argparse
+
 from elbepack.egpg import sign_file
 
 
 def run_command(argv):
-    if len(argv) != 2:
-        print('Wrong number of arguments.')
-        print('Please pass the name of the file to sign '
-              'and a valid gnupg fingerprint.')
-        return
-    sign_file(argv[0], argv[1])
+    parser = argparse.ArgumentParser(prog='elbe sign')
+    parser.add_argument('file', help='file to sign')
+    parser.add_argument('fingerprint', help='valid gnupg fingerprint')
+
+    args = parser.parse_args(argv)
+
+    sign_file(args.file, args.fingerprint)
