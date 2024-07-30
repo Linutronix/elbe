38f28b13d9ffc5c77a098b3785804fb287b9fa14
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-07-30 08:29:26 +0200

    elbepack: commands: drop optparse utility function
    It is not used anymore.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- elbepack/commands/__init__.py
+++ elbepack/commands/__init__.py
@@ -3,18 +3,7 @@
 # SPDX-FileCopyrightText: 2024 Linutronix GmbH
 
 import argparse
-import optparse
 import sys
-
-
-def _deprecated_option_cb(option, opt, value, parser):
-    print(f'Deprecated option "{option}" was used. This option is a NOOP.', file=sys.stderr)
-
-
-def add_deprecated_optparse_option(oparser, *args, **kwargs):
-    oparser.add_option(*args, **kwargs,
-                       action='callback', callback=_deprecated_option_cb,
-                       help=optparse.SUPPRESS_HELP)
 
 
 class _DeprecatedArgumentAction(argparse.Action):
