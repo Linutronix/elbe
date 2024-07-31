781f8b945a8d37ddd1619c54bb82727fe266155b
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-07-31 13:18:08 +0200

    elbepack: config: drop global configuration dict
    All configuration is now passed around explicitly.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- elbepack/config.py
+++ elbepack/config.py
@@ -7,30 +7,6 @@ import os
 from elbepack.cli import add_argument_to_parser_or_function
 
 
-class Config(dict):
-    def __init__(self):
-        dict.__init__(self)
-        self['soaphost'] = 'localhost'
-        self['soapport'] = '7587'
-        self['elbeuser'] = 'root'
-        self['elbepass'] = 'foo'
-
-        if 'ELBE_SOAPPORT' in os.environ:
-            self['soapport'] = os.environ['ELBE_SOAPPORT']
-
-        if 'ELBE_SOAPHOST' in os.environ:
-            self['soaphost'] = os.environ['ELBE_SOAPHOST']
-
-        if 'ELBE_USER' in os.environ:
-            self['elbeuser'] = os.environ['ELBE_USER']
-
-        if 'ELBE_PASS' in os.environ:
-            self['elbepass'] = os.environ['ELBE_PASS']
-
-
-cfg = Config()
-
-
 def add_argument_soaptimeout(parser):
     parser.add_argument(
         '--soaptimeout',
