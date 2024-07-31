4c25533fbd3ce3c021d44b9cd2b6260aa2094b91
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-07-31 10:11:48 +0200

    elbepack: init: split functionality into create_initvm()
    Separate the logic from the CLI handling.
    This makes the logic reusable without having to go through the CLI.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- elbepack/commands/init.py
+++ elbepack/commands/init.py
@@ -4,12 +4,10 @@
 
 import argparse
 import importlib.resources
-import logging
 import os
 import pathlib
 import shutil
 import subprocess
-import sys
 
 import elbepack.init
 from elbepack.commands import add_deprecated_argparse_argument
@@ -78,209 +76,203 @@ def run_command(argv):
     args = aparser.parse_args(argv)
 
     with elbe_logging({'files': None}):
-        if not args.skip_validation:
-            validation = validate_xml(args.xmlfile)
-            if validation:
-                logging.error('xml validation failed. Bailing out')
-                for i in validation:
-                    logging.error(i)
-                sys.exit(81)
+        create_initvm(
+            args.xmlfile,
+            args.directory,
+            skip_validation=args.skip_validation,
+            buildtype=args.buildtype,
+            cdrom=args.cdrom,
+            build_bin=args.build_bin,
+            build_sources=args.build_sources,
+            fail_on_warning=args.fail_on_warning,
+        )
 
-        xml = etree(args.xmlfile)
 
-        if not xml.has('initvm'):
-            logging.error('fatal error: '
-                          "xml missing mandatory section 'initvm'")
-            sys.exit(82)
+def create_initvm(xmlfile, directory, *,
+                  buildtype=None, skip_validation=False, cdrom=None, fail_on_warning=False,
+                  build_bin=True, build_sources=True):
+    if not skip_validation:
+        validation = validate_xml(xmlfile)
+        if validation:
+            raise ValueError(validation)
 
-        if args.buildtype:
-            buildtype = args.buildtype
-        elif xml.has('initvm/buildtype'):
-            buildtype = xml.text('/initvm/buildtype')
-        else:
-            buildtype = 'nodefaults'
+    xml = etree(xmlfile)
 
-        defs = ElbeDefaults(buildtype)
+    if not xml.has('initvm'):
+        raise ValueError("xml missing mandatory section 'initvm'")
 
-        http_proxy = xml.text('/initvm/mirror/primary_proxy', default='')
-        http_proxy = http_proxy.strip().replace('LOCALMACHINE', 'localhost')
+    if buildtype:
+        pass
+    elif xml.has('initvm/buildtype'):
+        buildtype = xml.text('/initvm/buildtype')
+    else:
+        buildtype = 'nodefaults'
 
-        if args.cdrom:
-            mirror = xml.node('initvm/mirror')
-            mirror.clear()
-            cdrom = mirror.ensure_child('cdrom')
-            cdrom.set_text(os.path.abspath(args.cdrom))
+    defs = ElbeDefaults(buildtype)
 
-        try:
-            os.makedirs(args.directory)
-        except OSError as e:
-            logging.error('unable to create project directory: %s (%s)',
-                          args.directory,
-                          e.strerror)
-            sys.exit(83)
+    http_proxy = xml.text('/initvm/mirror/primary_proxy', default='')
+    http_proxy = http_proxy.strip().replace('LOCALMACHINE', 'localhost')
 
-        out_path = os.path.join(args.directory, '.elbe-in')
-        try:
-            os.makedirs(out_path)
-        except OSError as e:
-            logging.error('unable to create subdirectory: %s (%s)',
-                          out_path,
-                          e.strerror)
-            sys.exit(84)
+    if cdrom:
+        mirror = xml.node('initvm/mirror')
+        mirror.clear()
+        cdrom_elem = mirror.ensure_child('cdrom')
+        cdrom_elem.set_text(os.path.abspath(cdrom))
 
-        initvm_http_proxy = http_proxy.replace('http://localhost:',
-                                               'http://10.0.2.2:')
-        elbe_exe = '/usr/bin/elbe'
-        if is_devel:
-            elbe_exe = '/var/cache/elbe/devel/elbe'
-        prj = xml.node('/initvm')
+    out_path = os.path.join(directory, '.elbe-in')
+    os.makedirs(out_path)
 
-        d = {'elbe_exe': elbe_exe,
-             'elbe_version': elbe_version,
-             'is_devel': is_devel,
-             'defs': defs,
-             'directory': args.directory,
-             'fail_on_warning': args.fail_on_warning,
-             'xml': xml,
-             'prj': prj,
-             'http_proxy': initvm_http_proxy,
-             'pkgs': xml.node('/initvm/pkg-list') or [],
-             'preseed': get_initvm_preseed(xml),
-             'cfg': cfg}
+    initvm_http_proxy = http_proxy.replace('http://localhost:',
+                                           'http://10.0.2.2:')
+    elbe_exe = '/usr/bin/elbe'
+    if is_devel:
+        elbe_exe = '/var/cache/elbe/devel/elbe'
+    prj = xml.node('/initvm')
 
-        if http_proxy != '':
-            os.putenv('http_proxy', http_proxy)
-            os.putenv('https_proxy', http_proxy)
-            os.putenv('no_proxy', 'localhost,127.0.0.1')
+    d = {'elbe_exe': elbe_exe,
+         'elbe_version': elbe_version,
+         'is_devel': is_devel,
+         'defs': defs,
+         'directory': directory,
+         'fail_on_warning': fail_on_warning,
+         'xml': xml,
+         'prj': prj,
+         'http_proxy': initvm_http_proxy,
+         'pkgs': xml.node('/initvm/pkg-list') or [],
+         'preseed': get_initvm_preseed(xml),
+         'cfg': cfg}
 
-        try:
-            copy_kinitrd(xml.node('/initvm'), out_path)
-        except NoKinitrdException as e:
-            msg = str(e)
-            logging.error('Failure to download kernel/initrd debian Package:')
-            logging.error('')
-            logging.error(msg)
-            logging.error('')
-            logging.error('Check Mirror configuration')
-            if 'SHA256SUMS' in msg:
-                logging.error('If you use debmirror please read '
-                              'https://github.com/Linutronix/elbe/issues/188 '
-                              'on how to work around the issue')
-            sys.exit(85)
+    if http_proxy != '':
+        os.putenv('http_proxy', http_proxy)
+        os.putenv('https_proxy', http_proxy)
+        os.putenv('no_proxy', 'localhost,127.0.0.1')
 
-        templates = [
-            ('init-elbe.sh.mako', out_path, True, False),
-            ('preseed.cfg.mako', out_path, True, False),
-            ('Makefile.mako', args.directory, False, True),
-            ('libvirt.xml.mako', args.directory, False, True),
-            ('apt.conf.mako', out_path, False, False),
-            ('default-init.xml', out_path, False, False),
+    try:
+        copy_kinitrd(xml.node('/initvm'), out_path)
+    except NoKinitrdException as e:
+        msg = (
+            'Failure to download kernel/initrd debian Package\n'
+            'Check Mirror configuration'
+        )
+        if 'SHA256SUMS' in e.msg:
+            msg += ('\nIf you use debmirror please read '
+                    'https://github.com/Linutronix/elbe/issues/188 '
+                    'on how to work around the issue')
+        raise ValueError(msg) from e
+
+    templates = [
+        ('init-elbe.sh.mako', out_path, True, False),
+        ('preseed.cfg.mako', out_path, True, False),
+        ('Makefile.mako', directory, False, True),
+        ('libvirt.xml.mako', directory, False, True),
+        ('apt.conf.mako', out_path, False, False),
+        ('default-init.xml', out_path, False, False),
+    ]
+
+    template_dir = importlib.resources.files(elbepack.init)
+
+    for t, out_dir, make_executable, linebreak in templates:
+        o = t.replace('.mako', '')
+
+        with importlib.resources.as_file(template_dir / t) as template:
+            write_template(
+                os.path.join(out_dir, o),
+                template, d, linebreak=linebreak,
+            )
+
+        if make_executable:
+            os.chmod(os.path.join(out_dir, o), 0o755)
+
+    shutil.copyfile(xmlfile,
+                    os.path.join(out_path, 'source.xml'))
+
+    keys = []
+    for key in xml.all('.//initvm/mirror/url-list/url/raw-key'):
+        keys.append(key.et.text)
+
+    if cdrom:
+        keys.append(subprocess.run([
+            '7z', 'x', '-so', cdrom, 'repo.pub',
+        ], check=True, capture_output=True, encoding='utf-8').stdout)
+
+    import_keyring = os.path.join(out_path, 'elbe-keyring')
+
+    do(f'gpg --no-options \
+             --no-default-keyring \
+             --keyring {import_keyring} --import',
+       input=''.join(keys).encode('ascii'),
+       check=False,
+       env_add={'GNUPGHOME': out_path})
+
+    export_keyring = import_keyring + '.gpg'
+
+    do(f'gpg --no-options \
+            --no-default-keyring \
+            --keyring {import_keyring} \
+            --export \
+            --output {export_keyring}',
+        env_add={'GNUPGHOME': out_path})
+
+    if is_devel:
+        opts = [
+            '--exclude-vcs',
+            '--exclude-vcs-ignores',
         ]
 
-        template_dir = importlib.resources.files(elbepack.init)
+        tar_fname = os.path.join(out_path, 'elbe-devel.tar.bz2')
+        source_dir = pathlib.Path(__file__).parents[2]
+        subprocess.run([
+            'tar', 'cfj', tar_fname, *opts, '-C', source_dir, 'elbepack', 'elbe'
+        ], check=True)
 
-        for t, out_dir, make_executable, linebreak in templates:
-            o = t.replace('.mako', '')
+    to_cpy = [('apt.conf', 'etc/apt'),
+              ('init-elbe.sh', ''),
+              ('source.xml', ''),
+              ('initrd-cdrom.gz', ''),
+              ('vmlinuz', ''),
+              ('preseed.cfg', '')]
 
-            with importlib.resources.as_file(template_dir / t) as template:
-                write_template(
-                    os.path.join(out_dir, o),
-                    template, d, linebreak=linebreak,
-                )
+    elbe_in = pathlib.Path(out_path)
 
-            if make_executable:
-                os.chmod(os.path.join(out_dir, o), 0o755)
+    if is_devel:
+        to_cpy.append(('elbe-devel.tar.bz2', ''))
 
-        shutil.copyfile(args.xmlfile,
-                        os.path.join(out_path, 'source.xml'))
+    # Convert relative rfs path to absolute in the system
+    to_cpy = [(elbe_in / src, elbe_in / 'initrd-tree' / dst)
+              for src, dst
+              in to_cpy]
 
-        keys = []
-        for key in xml.all('.//initvm/mirror/url-list/url/raw-key'):
-            keys.append(key.et.text)
+    # These are already absolute path!
+    keyrings = elbe_in / 'initrd-tree' / 'usr/share/keyrings'
+    for gpg in elbe_in.glob('*.gpg'):
+        to_cpy.append((gpg, keyrings))
 
-        if args.cdrom:
-            keys.append(subprocess.run([
-                '7z', 'x', '-so', args.cdrom, 'repo.pub',
-            ], check=True, capture_output=True, encoding='utf-8').stdout)
+    for src, dst in to_cpy:
+        try:
+            os.makedirs(dst)
+        except FileExistsError:
+            pass
+        shutil.copy(src, dst)
 
-        import_keyring = os.path.join(out_path, 'elbe-keyring')
+    elbe_in.joinpath('initrd-tree/usr/lib/finish-install.d').mkdir(parents=True, exist_ok=True)
 
-        do(f'gpg --no-options \
-                 --no-default-keyring \
-                 --keyring {import_keyring} --import',
-           input=''.join(keys).encode('ascii'),
-           check=False,
-           env_add={'GNUPGHOME': out_path})
+    def create_as_exec(file, flags):
+        return os.open(file, flags, mode=0o755)
 
-        export_keyring = import_keyring + '.gpg'
+    buildrepo_opts = ''
 
-        do(f'gpg --no-options \
-                --no-default-keyring \
-                --keyring {import_keyring} \
-                --export \
-                --output {export_keyring}',
-            env_add={'GNUPGHOME': out_path})
+    if not build_bin:
+        buildrepo_opts += '--skip-build-bin '
 
-        if is_devel:
-            opts = [
-                '--exclude-vcs',
-                '--exclude-vcs-ignores',
-            ]
+    if not build_sources:
+        buildrepo_opts += '--skip-build-source '
 
-            tar_fname = os.path.join(out_path, 'elbe-devel.tar.bz2')
-            source_dir = pathlib.Path(__file__).parents[2]
-            subprocess.run([
-                'tar', 'cfj', tar_fname, *opts, '-C', source_dir, 'elbepack', 'elbe'
-            ], check=True)
+    cdrom_opts = ''
+    if prj.has('mirror/cdrom'):
+        cdrom_opts = '--cdrom-device /dev/sr0 --cdrom-mount-path /media/cdrom0'
 
-        to_cpy = [('apt.conf', 'etc/apt'),
-                  ('init-elbe.sh', ''),
-                  ('source.xml', ''),
-                  ('initrd-cdrom.gz', ''),
-                  ('vmlinuz', ''),
-                  ('preseed.cfg', '')]
+    with open(elbe_in / 'initrd-tree/usr/lib/finish-install.d/93initvm-repo',
+              mode='x', opener=create_as_exec) as f:
 
-        elbe_in = pathlib.Path(out_path)
-
-        if is_devel:
-            to_cpy.append(('elbe-devel.tar.bz2', ''))
-
-        # Convert relative rfs path to absolute in the system
-        to_cpy = [(elbe_in / src, elbe_in / 'initrd-tree' / dst)
-                  for src, dst
-                  in to_cpy]
-
-        # These are already absolute path!
-        keyrings = elbe_in / 'initrd-tree' / 'usr/share/keyrings'
-        for gpg in elbe_in.glob('*.gpg'):
-            to_cpy.append((gpg, keyrings))
-
-        for src, dst in to_cpy:
-            try:
-                os.makedirs(dst)
-            except FileExistsError:
-                pass
-            shutil.copy(src, dst)
-
-        elbe_in.joinpath('initrd-tree/usr/lib/finish-install.d').mkdir(parents=True, exist_ok=True)
-
-        def create_as_exec(file, flags):
-            return os.open(file, flags, mode=0o755)
-
-        buildrepo_opts = ''
-
-        if not args.build_bin:
-            buildrepo_opts += '--skip-build-bin '
-
-        if not args.build_sources:
-            buildrepo_opts += '--skip-build-source '
-
-        cdrom_opts = ''
-        if prj.has('mirror/cdrom'):
-            cdrom_opts = '--cdrom-device /dev/sr0 --cdrom-mount-path /media/cdrom0'
-
-        with open(elbe_in / 'initrd-tree/usr/lib/finish-install.d/93initvm-repo',
-                  mode='x', opener=create_as_exec) as f:
-
-            f.write(f'in-target {elbe_exe} '
-                    f'fetch_initvm_pkgs {buildrepo_opts} {cdrom_opts} /var/cache/elbe/source.xml')
+        f.write(f'in-target {elbe_exe} '
+                f'fetch_initvm_pkgs {buildrepo_opts} {cdrom_opts} /var/cache/elbe/source.xml')
