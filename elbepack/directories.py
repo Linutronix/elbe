4aecc87bf3615aa0b8df0663f5c3fe516b88edad
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2024-08-08 12:04:08 +0200

    elbepack: directoris: remove run_elbe()
    This function is not used anymore, remove it.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- elbepack/directories.py
+++ elbepack/directories.py
@@ -3,14 +3,8 @@
 # SPDX-FileCopyrightText: 2015, 2017 Linutronix GmbH
 
 import os
-import subprocess
-import sys
 
 import elbepack.main
-
-
-def run_elbe(args, **kwargs):
-    return subprocess.run([sys.executable, '-melbepack', *args], **kwargs)
 
 
 def run_elbe_subcommand(args):
