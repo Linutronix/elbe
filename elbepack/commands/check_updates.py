Linutronix/elbe

commit c3b5e635177a29e485c6dc4e31fe4f3d929611ff

elbepack: check_updates: use helper for boolean attributes
Both "true" and "1" are valid values. Use the helper to recognize both.

Also drop the always useless assignment to pauto later.

Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

elbepack/commands/check_updates.py
@@ -78,9 +78,8 @@ def run_command(argv):
 
     for p in fullp:
         pname = p.et.text
-        pauto = p.et.get('auto')
 
-        if pauto != 'true':
+        if p.bool_attr('auto'):
             v.mark_install(pname)
 
     errors = 0
@@ -91,7 +90,6 @@ def run_command(argv):
     for p in fullp:
         xp = XMLPackage(p, arch)
         pname = p.et.text
-        pauto = p.et.get('auto')
 
         if not v.has_pkg(xp.name):
             if not xp.is_auto_installed:

commit 69972fd31d8f98acbde630c2651dbf7a0a44f8f1

elbepack: check_updates: fix check for "auto" attribute
Commit c3b5e635177a ("elbepack: check_updates: use helper for boolean
attributes") accidentally inverted the check for the "auto" attribute.
As a result only packages with this attribute would be taken into
account for the update checking.

Restore the original logic.

Fixes: c3b5e635177a ("elbepack: check_updates: use helper for boolean attributes")
Signed-off-by: Martin Kaistra <martin.kaistra@linutronix.de>
Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

elbepack/commands/check_updates.py
@@ -72,7 +72,7 @@ def run_command(argv):
     for p in fullp:
         pname = p.et.text
 
-        if p.bool_attr('auto'):
+        if not p.bool_attr('auto'):
             v.mark_install(pname)
 
     errors = 0