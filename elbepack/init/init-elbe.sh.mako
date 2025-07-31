Linutronix/elbe

commit 3c5923e2500ff2bdf2dcc5a3af0b7a959f3e4c17

elbepack: treeutils: introduce a helper to read apt keys from XML files
apt keys span multiple lines and may get misaligned during copy and
past. Elbe has logic to normalize the key strings but that is copied and
pasted everywhere. Unify it in a helper function.

Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

elbepack/init/init-elbe.sh.mako
@@ -4,6 +4,8 @@
 ##
 #! /bin/sh
 <%
+from elbepack.treeutils import strip_leading_whitespace_from_lines
+
 nicmac = prj.text('buildimage/NIC/MAC', default=defs, key='nicmac')
 %>
 
@@ -64,7 +66,7 @@ in-target haveged
 %   for node in prj.all("./finetuning/"):
 %     if "command" == node.tag:
 	cat <<ELBE_INITVM_FINETUNING_EOF > /buildenv/tmp/elbe-initvm
-${"\n".join(line.lstrip(" \t") for line in node.et.text.strip("\n").splitlines())}
+${split_leading_whitespace_from_lines(node.et.text)}
 ELBE_INITVM_FINETUNING_EOF
 	in-target sh /tmp/elbe-initvm
 %     endif

elbepack/finetuning.py
@@ -22,6 +22,7 @@ from elbepack.packers import default_packer, packers
 from elbepack.repomanager import UpdateRepo
 from elbepack.rpcaptcache import get_rpcaptcache
 from elbepack.shellhelper import chroot, do
+from elbepack.treeutils import strip_leading_whitespace_from_lines
 
 
 class FinetuningException(Exception):
@@ -244,8 +245,7 @@ class AddFileAction(FinetuningAction):
     @staticmethod
     def decode(text, encoding):
         if encoding == 'plain':
-            msg = '\n'.join([line.lstrip(' \t')
-                             for line in text.splitlines()[1:-1]])
+            msg = strip_leading_whitespace_from_lines(text)
         elif encoding == 'raw':
             msg = '\n'.join(text.splitlines()[1:-1])
         elif encoding == 'base64':
@@ -309,9 +309,7 @@ class CmdAction(ImageFinetuningAction):
 
     def execute_img(self, _buildenv, _target, builddir, loop_dev):
 
-        script = '\n'.join(line.lstrip(' \t')
-                           for line
-                           in self.node.et.text.strip('\n').splitlines())
+        script = strip_leading_whitespace_from_lines(self.node.et.text)
 
         mnt = os.path.join(builddir, 'imagemnt')
         dev = f"{loop_dev}p{self.node.et.attrib['part']}"

elbepack/pbuilder.py
@@ -6,6 +6,8 @@ import os
 import pathlib
 from urllib.request import urlopen
 
+from elbepack.treeutils import strip_leading_whitespace_from_lines
+
 
 def _write_env(fp, k, v):
     fp.write(f'{k}="{v}"\n')
@@ -180,11 +182,7 @@ def get_apt_keys(builddir, xml):
                 continue
 
             if url.has('raw-key'):
-
-                key = '\n'.join(line.strip(' \t')
-                                for line
-                                in url.text('raw-key').splitlines()[1:-1])
-
-                keys.append((f'elbe-xml-raw-key{i}', key))
+                keys.append((f'elbe-xml-raw-key{i}',
+                             strip_leading_whitespace_from_lines(url.text('raw-key'))))
 
     return keys

elbepack/rfs.py
@@ -12,6 +12,7 @@ from elbepack.efilesystem import ChRootFilesystem, dpkg_architecture
 from elbepack.egpg import unarmor_openpgp_keyring
 from elbepack.shellhelper import chroot, do
 from elbepack.templates import get_preseed, preseed_to_text, write_pack_template
+from elbepack.treeutils import strip_leading_whitespace_from_lines
 
 
 def create_apt_prefs(xml, rfs):
@@ -283,7 +284,7 @@ class BuildEnv:
             # I could make a none global 'noauth' flag for mirrors
             for i, url in enumerate(self.xml.node('project/mirror/url-list')):
                 if url.has('raw-key'):
-                    key = '\n'.join(line.strip(' \t') for line in url.text('raw-key').splitlines())
+                    key = strip_leading_whitespace_from_lines(url.text('raw-key'))
                     self.add_key(unarmor_openpgp_keyring(key), f'elbe-xml-raw-key{i}.gpg')
 
     def initialize_dirs(self, build_sources=False):

elbepack/treeutils.py
@@ -37,6 +37,10 @@ def dbsfed_schema():
     return XMLSchema(schema_tree)
 
 
+def strip_leading_whitespace_from_lines(s):
+    return '\n'.join(line.strip(' \t') for line in s.strip().splitlines())
+
+
 class eiter:
     def __init__(self, it):
         self.it = it

elbepack/virtapt.py
@@ -16,6 +16,7 @@ import apt_pkg
 from elbepack.egpg import unarmor_openpgp_keyring
 from elbepack.filesystem import TmpdirFilesystem
 from elbepack.rfs import create_apt_prefs
+from elbepack.treeutils import strip_leading_whitespace_from_lines
 
 
 class VirtApt:
@@ -104,9 +105,9 @@ class VirtApt:
             # I could make a none global 'noauth' flag for mirrors
             for i, url in enumerate(self.xml.node('project/mirror/url-list')):
                 if url.has('raw-key'):
-                    key = '\n'.join(line.strip(' \t')
-                                    for line in url.text('raw-key').splitlines()[1:-1])
-                    self._add_key(unarmor_openpgp_keyring(key), f'elbe-virtapt-raw-key{i}.gpg')
+                    key = strip_leading_whitespace_from_lines(url.text('raw-key'))
+                    self._add_key(unarmor_openpgp_keyring(key),
+                                  f'elbe-virtapt-raw-key{i}.gpg')
 
     def _initialize_dirs(self):
         self.basefs.mkdir_p('cache/archives/partial')

commit 3773b572df5ce1a4c0107b8bddcc62f0e39963d7

elbepack: init-elbe.sh: fix function name typo in initvm finetuning
The function name was missspelled, fix it.

Fixes: 3c5923e2500f ("elbepack: treeutils: introduce a helper to read apt keys from XML files")
Closes: https://github.com/Linutronix/elbe/issues/440
Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

elbepack/init/init-elbe.sh.mako
@@ -66,7 +66,7 @@ in-target haveged
 %   for node in prj.all("./finetuning/"):
 %     if "command" == node.tag:
 	cat <<ELBE_INITVM_FINETUNING_EOF > /buildenv/tmp/elbe-initvm
-${split_leading_whitespace_from_lines(node.et.text)}
+${strip_leading_whitespace_from_lines(node.et.text)}
 ELBE_INITVM_FINETUNING_EOF
 	in-target sh /tmp/elbe-initvm
 %     endif