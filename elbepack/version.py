Linutronix/elbe

commit e28c2758359146cd0034d90b643e8b22d1671d2c

elbepack: version: correctly detect is_devel if installed in user directory
When ELBE is installed into a user site directory, is_devel should be False.
This is happens for example through 'pip install --user'.

Closes: https://github.com/Linutronix/elbe/issues/439
Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

elbepack/version.py
@@ -3,10 +3,12 @@
 # SPDX-FileCopyrightText: 2013-2018 Linutronix GmbH
 
 import pathlib
+import site
 import sys
 
 
-is_devel = not pathlib.Path(__file__).is_relative_to(sys.prefix)
+_filepath = pathlib.Path(__file__)
+is_devel = not _filepath.is_relative_to(sys.prefix) and not _filepath.is_relative_to(site.USER_SITE)
 elbe_version = '15.6'
 elbe_version_debian = elbe_version
 if is_devel:

commit b0b7f1a494d1918063e1b0869ceb066518aa54ef

elbepack: version: correctly handle site.USER_SITE being None
site.USER_SITE may be None. Handle this gracefully.

Fixes: e28c27583591 ("elbepack: version: correctly detect is_devel if installed in user directory")
Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

elbepack/version.py
@@ -7,8 +7,18 @@ import site
 import sys
 
 
+def _is_devel(p):
+    if p.is_relative_to(sys.prefix):
+        return False
+
+    if site.USER_SITE is not None and p.is_relative_to(site.USER_SITE):
+        return False
+
+    return True
+
+
 _filepath = pathlib.Path(__file__)
-is_devel = not _filepath.is_relative_to(sys.prefix) and not _filepath.is_relative_to(site.USER_SITE)
+is_devel = _is_devel(_filepath)
 elbe_version = '15.6'
 elbe_version_debian = elbe_version
 if is_devel: