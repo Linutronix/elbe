7278b6ec1709ff8b023d58749f8cf3211a982358
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2026-01-27 14:48:11 +0100

    website: add favicon
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- website/conf.py
+++ website/conf.py
@@ -94,6 +94,8 @@ html_theme_options = {
         'footer_text': '#3E4349',
 }
 
+html_favicon = '_static/favicon.ico'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
