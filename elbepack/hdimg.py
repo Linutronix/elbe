Linutronix/elbe

commit 1fe983101aa7eb225c6c2f32d293b4741445637d

elbepack: hdimg: pass mount options
For some use cases, like compression, it's essential to apply the mount
options not only during runtime, but also during creation of the images.

Signed-off-by: Daniel Braunwarth <daniel.braunwarth@kuka.com>
Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

elbepack/hdimg.py
@@ -164,7 +164,9 @@ class grubinstaller202(grubinstaller_base):
 
                 for entry in self.fs.depthlist():
                     stack.enter_context(
-                        mount(f'{loopdev}p{entry.partnum}', imagemntfs.fname(entry.mountpoint)))
+                        mount(f'{loopdev}p{entry.partnum}',
+                              imagemntfs.fname(entry.mountpoint),
+                              options=entry.options))
 
                 for bindmnt in ['/dev', '/proc', '/sys']:
                     stack.enter_context(
@@ -215,7 +217,9 @@ class grubinstaller97(grubinstaller_base):
                         bootentry_label = entry.label
                         bootentry = int(entry.partnum)
                     stack.enter_context(
-                        mount(f'{loopdev}p{entry.partnum}', imagemntfs.fname(entry.mountpoint)))
+                        mount(f'{loopdev}p{entry.partnum}',
+                              imagemntfs.fname(entry.mountpoint),
+                              options=entry.options))
 
                 if not bootentry:
                     bootentry_label = entry.label
@@ -308,7 +312,7 @@ def create_label(disk, part, ppart, fslabel, target, grub):
 
         mount_path = Path(target, 'imagemnt')
 
-        with mount(loopdev, mount_path):
+        with mount(loopdev, mount_path, options=entry.options):
             _execute_fs_commands(entry.fs_path_commands, dict(path=mount_path))
             do([
                 'cp', '-a',

elbepack/imgutils.py
@@ -58,7 +58,7 @@ class _Mount:
             cmd.append('--bind')
 
         if options is not None:
-            cmd.extend(['-o', ','.join(options)])
+            cmd.extend(['-o', options])
 
         if type is not None:
             cmd.extend(['-t', type])

commit 20973b4114c31ca531a9c91b9d2c46fea677879b

elbepack: hdimg: always mount filesystem writable during image creation
When copying the files to the target filesystems, these need to be
mounted as writable, even if on the final system they are read-only.

Fixes: 1fe983101aa7 ("elbepack: hdimg: pass mount options")
Closes: https://github.com/Linutronix/elbe/issues/458
Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

elbepack/hdimg.py
@@ -166,7 +166,7 @@ class grubinstaller202(grubinstaller_base):
                     stack.enter_context(
                         mount(f'{loopdev}p{entry.partnum}',
                               imagemntfs.fname(entry.mountpoint),
-                              options=entry.options))
+                              options=entry.options, force_writable=True))
 
                 for bindmnt in ['/dev', '/proc', '/sys']:
                     stack.enter_context(
@@ -219,7 +219,7 @@ class grubinstaller97(grubinstaller_base):
                     stack.enter_context(
                         mount(f'{loopdev}p{entry.partnum}',
                               imagemntfs.fname(entry.mountpoint),
-                              options=entry.options))
+                              options=entry.options, force_writable=True))
 
                 if not bootentry:
                     bootentry_label = entry.label
@@ -312,7 +312,7 @@ def create_label(disk, part, ppart, fslabel, target, grub):
 
         mount_path = Path(target, 'imagemnt')
 
-        with mount(loopdev, mount_path, options=entry.options):
+        with mount(loopdev, mount_path, options=entry.options, force_writable=True):
             _execute_fs_commands(entry.fs_path_commands, dict(path=mount_path))
             do([
                 'cp', '-a',

elbepack/imgutils.py
@@ -49,7 +49,8 @@ class _Mount:
     # This is not using contextlib.contextmanager as it will be pass to our
     # RPCAPTCache which uses the pickle serialization.
     # The generator by contextlib.contextmanager is not compatible with pickle.
-    def __init__(self, device, target, *, bind=False, type=None, options=None, log_output=True):
+    def __init__(self, device, target, *, bind=False, type=None, options=None, log_output=True,
+                 force_writable=False):
         self.log_output = log_output
         self.target = target
 
@@ -59,6 +60,9 @@ class _Mount:
 
         if options is not None:
             cmd.extend(['-o', options])
+
+        if force_writable:
+            cmd.append('--rw')
 
         if type is not None:
             cmd.extend(['-t', type])