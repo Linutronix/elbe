bc12c0a3b23e2bc1da8d6a9ff17f013892dcaea3
Florian Kauer <florian.kauer@linutronix.de>
2026-07-01 13:26:06 +0200

    elbepack: separate common logic from VM logic
    elbepack/initvm(action).py currently includes implementations that can
    be shared with a future VM-less and SOAP-less build command.
    
    Extract these into a separate module.
    
    This commit only moves code around. No functional change.
    
    Signed-off-by: Florian Kauer <florian.kauer@linutronix.de>
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

---
+++ elbepack/buildsubmitaction.py
@@ -0,0 +1,82 @@
+# ELBE - Debian Based Embedded Rootfilesystem Builder
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2026 Linutronix GmbH
+
+import os
+import subprocess
+import sys
+import textwrap
+
+from elbepack.cli import CliError, add_argument, with_cli_details
+from elbepack.elbexml import ElbeXML, ValidationError
+from elbepack.filesystem import TmpdirFilesystem
+
+
+def extract_cdrom(cdrom):
+    """ Extract cdrom iso image
+        returns a TmpdirFilesystem() object containing
+        the source.xml, which is also validated.
+    """
+
+    tmp = TmpdirFilesystem()
+    in_iso_name = 'source.xml'
+    try:
+        import pycdlib
+        iso = pycdlib.PyCdlib()
+        iso.open(cdrom)
+        extracted = os.path.join(tmp.path, in_iso_name)
+        iso.get_file_from_iso(extracted, iso_path=f'/{in_iso_name.upper()};1')
+        iso.close()
+    except ImportError:
+        subprocess.run(['7z', 'x', f'-o{tmp.path}', cdrom, in_iso_name], check=True)
+
+    print('', file=sys.stderr)
+
+    if not tmp.isfile('source.xml'):
+        raise CliError(140, textwrap.dedent("""
+            Iso image does not contain a source.xml file.
+            This is not supported by 'elbe initvm'."""))
+
+    try:
+        exml = ElbeXML(tmp.fname('source.xml'))
+    except ValidationError as e:
+        raise with_cli_details(e, 141, textwrap.dedent("""
+            Iso image does contain a source.xml file.
+            But that xml does not validate correctly."""))
+
+    print('Iso Image with valid source.xml detected !')
+    print(f'Image was generated using Elbe Version {exml.get_elbe_version()}')
+
+    return tmp
+
+
+def add_submit_arguments(f):
+    f = add_argument('--skip-download', action='store_true',
+                     dest='skip_download', default=False,
+                     help='Skip downloading generated Files')(f)
+
+    f = add_argument('--output', dest='outdir',
+                     type=os.path.abspath,
+                     help='directory where to save downloaded Files')(f)
+
+    f = add_argument('--skip-build-bin', dest='build_bin', action='store_false', default=True,
+                     help='Skip building Binary Repository CDROM, for exact Reproduction')(f)
+
+    f = add_argument('--skip-build-sources', action='store_false',
+                     dest='build_sources', default=True,
+                     help='Skip building Source CDROM')(f)
+
+    f = add_argument('--keep-files', action='store_true',
+                     dest='keep_files', default=False,
+                     help="don't delete elbe project files in initvm")(f)
+
+    f = add_argument('--writeproject', dest='writeproject', default=None,
+                     help='write project name to file')(f)
+
+    f = add_argument('--build-sdk', dest='build_sdk', action='store_true', default=False,
+                     help="Also make 'initvm submit' build an SDK.")(f)
+
+    f = add_argument('--base-image', dest='base_image',
+                     help='Use a base image instead of debootstrap (experimental)')(f)
+
+    return f
--- elbepack/initvmaction.py
+++ elbepack/initvmaction.py
@@ -12,10 +12,11 @@ import time
 
 import elbepack
 import elbepack.initvm
+from elbepack.buildsubmitaction import add_submit_arguments, extract_cdrom
 from elbepack.cli import CliError, add_argument, with_cli_details
 from elbepack.config import add_argument_sshport, add_arguments_soapclient
-from elbepack.elbexml import ElbeXML, ValidationError
-from elbepack.filesystem import TmpdirFilesystem, size_to_int
+from elbepack.elbexml import ValidationError
+from elbepack.filesystem import size_to_int
 from elbepack.init import create_initvm
 from elbepack.paths import DEVEL_DIR
 from elbepack.repodir import Repodir, RepodirError
@@ -221,81 +222,11 @@ def _submit_and_dl_result(control, xmlfile, cdrom, base_image, args, xmlfile_bas
             control.service.del_project(prjdir)
 
 
-def _extract_cdrom(cdrom):
-    """ Extract cdrom iso image
-        returns a TmpdirFilesystem() object containing
-        the source.xml, which is also validated.
-    """
-
-    tmp = TmpdirFilesystem()
-    in_iso_name = 'source.xml'
-    try:
-        import pycdlib
-        iso = pycdlib.PyCdlib()
-        iso.open(cdrom)
-        extracted = os.path.join(tmp.path, in_iso_name)
-        iso.get_file_from_iso(extracted, iso_path=f'/{in_iso_name.upper()};1')
-        iso.close()
-    except ImportError:
-        subprocess.run(['7z', 'x', f'-o{tmp.path}', cdrom, in_iso_name], check=True)
-
-    print('', file=sys.stderr)
-
-    if not tmp.isfile('source.xml'):
-        raise CliError(140, textwrap.dedent("""
-            Iso image does not contain a source.xml file.
-            This is not supported by 'elbe initvm'."""))
-
-    try:
-        exml = ElbeXML(tmp.fname('source.xml'))
-    except ValidationError as e:
-        raise with_cli_details(e, 141, textwrap.dedent("""
-            Iso image does contain a source.xml file.
-            But that xml does not validate correctly."""))
-
-    print('Iso Image with valid source.xml detected !')
-    print(f'Image was generated using Elbe Version {exml.get_elbe_version()}')
-
-    return tmp
-
-
-def _add_submit_arguments(f):
-    f = add_argument('--skip-download', action='store_true',
-                     dest='skip_download', default=False,
-                     help='Skip downloading generated Files')(f)
-
-    f = add_argument('--output', dest='outdir',
-                     type=os.path.abspath,
-                     help='directory where to save downloaded Files')(f)
-
-    f = add_argument('--skip-build-bin', dest='build_bin', action='store_false', default=True,
-                     help='Skip building Binary Repository CDROM, for exact Reproduction')(f)
-
-    f = add_argument('--skip-build-sources', action='store_false',
-                     dest='build_sources', default=True,
-                     help='Skip building Source CDROM')(f)
-
-    f = add_argument('--keep-files', action='store_true',
-                     dest='keep_files', default=False,
-                     help="don't delete elbe project files in initvm")(f)
-
-    f = add_argument('--writeproject', dest='writeproject', default=None,
-                     help='write project name to file')(f)
-
-    f = add_argument('--build-sdk', dest='build_sdk', action='store_true', default=False,
-                     help="Also make 'initvm submit' build an SDK.")(f)
-
-    f = add_argument('--base-image', dest='base_image',
-                     help='Use a base image instead of debootstrap (experimental)')(f)
-
-    return f
-
-
 @_add_initvm_from_args_arguments
 @add_argument('--fail-on-warning', action='store_true',
               dest='fail_on_warning', default=False,
               help=argparse.SUPPRESS)
-@_add_submit_arguments
+@add_submit_arguments
 @add_argument('--size', help='Disk size', type=size_to_int)
 @add_argument('input', nargs='?', metavar='<xmlfile> | <isoimage>')
 def _create(args):
@@ -328,7 +259,7 @@ def _create(args):
 
         elif args.input.endswith('.iso'):
             # We have an iso image, extract xml from there.
-            tmp = _extract_cdrom(args.input)
+            tmp = extract_cdrom(args.input)
 
             xmlfile = tmp.fname('source.xml')
             cdrom = args.input
@@ -384,7 +315,7 @@ def _create(args):
 
 
 @_add_initvm_from_args_arguments
-@_add_submit_arguments
+@add_submit_arguments
 @add_argument('input', metavar='<xmlfile> | <isoimage>')
 def _submit(args):
     initvm = _initvm_from_args(args)
@@ -399,7 +330,7 @@ def _submit(args):
         xmlfile = args.input
     elif args.input.endswith('.iso'):
         # We have an iso image, extract xml from there.
-        tmp = _extract_cdrom(args.input)
+        tmp = extract_cdrom(args.input)
         xmlfile = tmp.fname('source.xml')
         cdrom = args.input
     else:
