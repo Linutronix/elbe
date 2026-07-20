8973f45505ac1076a2afb221dab4faa7fd2d69a8
Florian Kauer <florian.kauer@linutronix.de>
2026-07-20 11:08:11 +0200

    elbepack: test module import
    Test the import of the Python modules to ensure everything can be
    imported cleanly even with -Werror.
    
    Signed-off-by: Florian Kauer <florian.kauer@linutronix.de>

---
+++ elbepack/tests/test_import.py
@@ -0,0 +1,24 @@
+# ELBE - Debian Based Embedded Rootfilesystem Builder
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2026 Linutronix GmbH
+
+import importlib
+import pkgutil
+
+import pytest
+
+import elbepack
+
+modules = [
+    module.name
+    for module in pkgutil.walk_packages(elbepack.__path__, elbepack.__name__ + '.')
+    if not module.name.endswith('.__main__')
+]
+
+
+@pytest.mark.parametrize('module', modules)
+def test_import(module):
+    try:
+        importlib.import_module(module)
+    except ImportError:
+        pass
