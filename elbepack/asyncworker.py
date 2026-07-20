Linutronix/elbe

commit a747152fc2156e07bf58e7c9e78c02567311834e

elbepack: soap: remove beaker middleware
No authentication is performed anymore, so the beaker middleware is
unused.

Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

elbepack/daemons/soap/__init__.py
@@ -5,8 +5,6 @@
 import logging
 import warnings
 
-from beaker.middleware import SessionMiddleware
-
 # spyne uses a bundled version of six, which triggers warnings in spyne version 2.14.0.
 # As the warnings can happen during any import, a scoped .catch_warnings() is not enough.
 if True:  # avoid flake8 errors for the following imports
@@ -30,12 +28,6 @@ class EsoapApp(Application):
         super().__init__(*args, **kargs)
         self.pm = ProjectManager('/var/cache/elbe')
 
-
-class MySession(SessionMiddleware):
-    def __init__(self, app, pm):
-        self.pm = pm
-        super().__init__(app)
-
     def stop(self):
         self.pm.stop()
 
@@ -46,5 +38,4 @@ def get_app():
                    in_protocol=Soap11(validator='lxml'),
                    out_protocol=Soap11())
 
-    wsgi = WsgiApplication(app)
-    return MySession(wsgi, app.pm)
+    return WsgiApplication(app)

debian/control
@@ -101,8 +101,7 @@ Depends: ${misc:Depends}, ${python3:Depends},
   python3,
   python3-elbe-bin (= ${binary:Version}),
   python3-elbe-common (= ${binary:Version}),
-  python3-elbe-buildenv (= ${binary:Version}),
-  python3-beaker
+  python3-elbe-buildenv (= ${binary:Version})
 Description: wsgi daemon interface
  subcommand to start wsgi services.
 

commit deccb3ed50e74c9abb26befef1cdfefcde82b51b

elbepack: soap: restore stop method accessibility
Commit a747152fc ("elbepack: soap: remove beaker middleware") removed
the MySession wrapper class that provided access to the stop method.
This caused the daemon management code to no longer find and execute
the stop method during shutdown.

This restores the proper shutdown behavior where the stop method is
called during daemon termination.

Also, the stop of the AsyncWorker was never finalized because join
waited for task_done after pushing None.

Fixes: a747152fc ("elbepack: soap: remove beaker middleware")
Signed-off-by: Florian Kauer <florian.kauer@linutronix.de>

elbepack/asyncworker.py
@@ -299,6 +299,7 @@ class AsyncWorker(Thread):
         while True:
             job = self.queue.get()
             if job is None:
+                self.queue.task_done()
                 break
 
             os.chdir('/')

elbepack/daemons/soap/__init__.py
@@ -32,10 +32,19 @@ class EsoapApp(Application):
         self.pm.stop()
 
 
+class StopableWsgiApplication(WsgiApplication):
+    def __init__(self, app):
+        self._app = app
+        super().__init__(app)
+
+    def stop(self):
+        self._app.stop()
+
+
 def get_app():
 
     app = EsoapApp([ESoap], 'soap',
                    in_protocol=Soap11(validator='lxml'),
                    out_protocol=Soap11())
 
-    return WsgiApplication(app)
+    return StopableWsgiApplication(app)