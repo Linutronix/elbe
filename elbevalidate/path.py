9e5a7a907c0fa7a669d5097bfddefb364a5e5942
Alexander Kanavin <alex@linutronix.de>
2026-08-05 12:58:16 +0200

    elbevalidate/path: add a tarball implementation
    This allows looking into root filesystems that are packaged
    as tarballs (in addition to an implementation that looks into
    disk images using guestfs), and is useful for testing the content
    of base images.
    
    Note that this is a 'lazy' implementation; I have added only
    the methods that are actually called by tests. More can be added
    if/when needed.
    
    Signed-off-by: Alexander Kanavin <alex@linutronix.de>

--- elbevalidate/path.py
+++ elbevalidate/path.py
@@ -220,3 +220,52 @@ class ImagePath(Path):
             stat['files'], stat['ffree'], stat['favail'],
             stat['fsid'], stat['flag'], stat['namemax'],
         ])
+
+
+class TarballPath(Path):
+    """
+    Reference to a path inside a :py:class:`tarfile.TarFile`.
+
+    For documentation see :py:mod:`pathlib`.
+    """
+    def __init__(self, *pathsegments, tar):
+        self.tar = tar
+        self._p = pathlib.PurePosixPath(*pathsegments)
+
+    def _create_from_posixpath(self, p):
+        return type(self)(
+                p,
+                tar=self.tar,
+        )
+
+    def _info(self):
+        return self.tar.getmember('.' + self._path)
+
+    def read_bytes(self):
+        return self.tar.extractfile('.' + self._path).read()
+
+    def exists(self):
+        return ('.' + self._path) in self.tar.getnames()
+
+    def is_file(self):
+        return self._info().isfile()
+
+    def stat(self):
+        info = self._info()
+        return os.stat_result((
+            info.mode, 0, 0, 0, info.uid, info.gid, info.size, info.mtime, info.mtime, info.mtime))
+
+    def is_dir(self):
+        return self._info().isdir()
+
+    def iterdir(self):
+        dir = '.' + self._path
+        for entry in self.tar.getnames():
+            if os.path.dirname(entry) == dir:
+                yield self._create_from_posixpath(entry)
+
+    def is_symlink(self):
+        return self._info().issym()
+
+    def readlink(self):
+        return self._info().linkname
